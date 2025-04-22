464e56b83e3bd04f8b644eeb8e5f1de58759e745
@hexfa <amir.dehdarian2009@gmail.com>
2025-04-22 07:01:04 -0700

    feat(navigation): add composable routes for Home, Basket, and Profile screens add profileScreen,basketScreen,homeScreen

--- app/src/main/java/com/example/shop/navigation/NavGraph.kt
+++ app/src/main/java/com/example/shop/navigation/NavGraph.kt
@@ -4,6 +4,9 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.example.shop.ui.screens.BasketScreen
+import com.example.shop.ui.screens.HomeScreen
+import com.example.shop.ui.screens.ProfileScreen
 import com.example.shop.ui.screens.SplashScreen
 
 @Composable
@@ -13,6 +16,19 @@ fun SetUpNavGraph(navController: NavHostController){
     ){
         composable(route =Screen.Splash.route ){
             SplashScreen(navController=navController)
+        }
+
+        composable(route=Screen.Home.route){
+            HomeScreen(navController=navController)
+        }
+
+        composable(route=Screen.Basket.route){
+
+            BasketScreen(navController=navController)
+        }
+
+        composable(route=Screen.Profile.route){
+            ProfileScreen(navController=navController)
         }
     }
 }
---
+++ app/src/main/java/com/example/shop/ui/screens/BasketScreen.kt
@@ -0,0 +1,10 @@
+package com.example.shop.ui.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun BasketScreen(navController: NavHostController){
+    Text("BasketScreen")
+}
