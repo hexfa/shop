b4ff3728f033f22f4632a45e4a694ed4fa60def8
@hexfa <amir.dehdarian2009@gmail.com>
2025-04-22 07:39:26 -0700

    feat(theme): add app color palette and splashBg extension
    - Define primary and accent colors (Purple80, PurpleGrey80, Pink80, Purple40, PurpleGrey40, Pink40)
    - Add Colors.splashBg extension for splash background color

--- app/src/main/java/com/example/shop/ui/screens/SplashScreen.kt
+++ app/src/main/java/com/example/shop/ui/screens/SplashScreen.kt
@@ -1,12 +1,29 @@
 package com.example.shop.ui.screens
 
 import android.window.SplashScreen
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import com.example.shop.ui.theme.splashBg
+
 
 @Composable
 fun SplashScreen(navController: NavHostController){
     Text("SplashScreen")
 
+}
+
+@Composable
+fun Splash(){
+    Box(modifier = Modifier.background(MaterialTheme.colorScheme.splashBg).fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ){
+
+    }
 }
--- app/src/main/java/com/example/shop/ui/theme/Color.kt
+++ app/src/main/java/com/example/shop/ui/theme/Color.kt
@@ -1,11 +1,16 @@
 package com.example.shop.ui.theme
-
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.material3.ColorScheme as Colors
 
-val Purple80 = Color(0xFFD0BCFF)
+val Purple80 = Color(0xFFed1b34)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Colors.splashBg : Color
+   @Composable
+   get()=Color(0xFFed1b34)
