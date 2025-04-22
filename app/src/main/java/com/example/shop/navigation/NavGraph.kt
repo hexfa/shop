bb5744eb28d2dbc5004adecaeece758a1553cf6f
@hexfa <amir.dehdarian2009@gmail.com>
2025-04-22 06:32:11 -0700

    explain splashScreen in composable function

--- app/src/main/java/com/example/shop/navigation/NavGraph.kt
+++ app/src/main/java/com/example/shop/navigation/NavGraph.kt
@@ -3,12 +3,15 @@ package com.example.shop.navigation
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 
 @Composable
 fun SetUpNavGraph(navController: NavHostController){
     NavHost(navController = navController,
         startDestination =Screen.splash.rout
     ){
+        composable(route =Screen.splash.rout ){
 
+        }
     }
 }
