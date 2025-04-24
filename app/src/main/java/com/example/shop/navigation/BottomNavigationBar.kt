ec428913bab7467201293a5e21994f76b2107e1b
@hexfa <amir.dehdarian2009@gmail.com>
2025-04-24 05:14:07 -0700

    feat(theme): define custom `headlineMedium` TextStyle

--- app/src/main/java/com/example/shop/navigation/BottomNavigationBar.kt
+++ app/src/main/java/com/example/shop/navigation/BottomNavigationBar.kt
@@ -10,11 +10,13 @@ import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
@@ -89,8 +91,13 @@ fun BottomNavigationBar(
                                 Icon(painter = item.deSelectedIcon  , contentDescription = item.name, modifier = Modifier.height(24.dp))
 
                             }
+
+                            Text(text = item.name,
+                                textAlign = TextAlign.Center)
                         }
                     }
+
+
                 )
             }
         }
