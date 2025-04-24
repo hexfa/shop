355eacf72cc24375ff7988a9f6dd2a2dfce00e17
@hexfa <amir.dehdarian2009@gmail.com>
2025-04-24 06:25:16 -0700

    feat(typography): register `font_medium` FontFamily with IRANYekanMedium

--- app/src/main/java/com/example/shop/ui/theme/Type.kt
+++ app/src/main/java/com/example/shop/ui/theme/Type.kt
@@ -3,9 +3,19 @@ package com.example.shop.ui.theme
 //noinspection UsingMaterialAndMaterial3Libraries
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.shop.R
+
+
+val font_medium= FontFamily(
+    Font(R.font.iranyekanmedium)
+)
+
+
+
 
 // Set of Material typography styles to start with
 val Typography = Typography(
