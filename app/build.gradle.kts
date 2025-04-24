4ae48cb152de740e01c7d5d92cbeeebbe09ab773
@hexfa <amir.dehdarian2009@gmail.com>
2025-04-24 04:17:30 -0700

    refactor(ui): migrate BottomNavigation → NavigationBar (Material )

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,8 +53,12 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.material)
+
     implementation(libs.androidx.compose.material)
     implementation(libs.androidx.navigation.runtime.ktx)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -97,4 +101,5 @@ dependencies {
 
     // system ui controller
     implementation(libs.accompanist.systemuicontroller)
+
 }
--- app/src/main/java/com/example/shop/navigation/BottomNavigationBar.kt
+++ app/src/main/java/com/example/shop/navigation/BottomNavigationBar.kt
@@ -1,29 +1,19 @@
 package com.example.shop.navigation
 
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 //noinspection UsingMaterialAndMaterial3Libraries
-import androidx.compose.material.*
+import androidx.compose.material.BottomNavigation
+//noinspection UsingMaterialAndMaterial3Libraries
+import androidx.compose.material.BottomNavigationItem
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
-import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import com.example.shop.R
-
+//noinspection UsingMaterialAndMaterial3Libraries
 
 @Composable
 fun BottomNavigationBar(
@@ -65,21 +55,22 @@ fun BottomNavigationBar(
     val backStackEntry=navController.currentBackStackEntryAsState()
     val  showBottomBar=backStackEntry.value?.destination?.route in items.map { it.route }
 
-    /*if(showBottomBar){
+    if(showBottomBar){
+
         BottomNavigation(
             modifier=Modifier,
-            backGroundColor=Color.White,
+            backgroundColor =Color.White,
             elevation=5.dp
         ){
 
             items.forEachIndexed{index,item->
 
                 val selected=item.route==backStackEntry.value?.destination?.route
-                BottomNavigationItem(selected=selected,onClick=onItemClick(item)}){
+               // BottomNavigationItem(selected=selected,onClick=onItemClick(item)}){
                     //unSelectedCenterColor=
             }
         }
-    }*/
+    }
 
 
 }
