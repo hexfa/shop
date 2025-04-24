a3385ca343202793a08c3bbe9287bb1631be0db3
@hexfa <amir.dehdarian2009@gmail.com>
2025-04-24 06:32:57 -0700

    feat(theme): customize TextStyles with new Yekan font families
    • Updated Typography:
      • body1 → font_medium (IRANYekanMedium), 16 sp, lh 25 sp, ls 0.5 sp
      • body2 → font_standard (IRANYekanRegular), 14 sp, lh 25 sp, ls 0.5 sp
      • h1    → font_standard, Medium weight, 20 sp, lh 25 sp

--- app/src/main/java/com/example/shop/ui/theme/Type.kt
+++ app/src/main/java/com/example/shop/ui/theme/Type.kt
@@ -28,16 +28,27 @@ val font_standard= FontFamily(
 // Set of Material typography styles to start with
 val Typography = Typography(
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontFamily = font_medium,
+        fontWeight = FontWeight.Light,
         fontSize = 16.sp,
-        lineHeight = 24.sp,
+        lineHeight = 25.sp,
         letterSpacing = 0.5.sp
     ),
+
+    body2 = TextStyle(
+        fontFamily = font_standard,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 25.sp,
+        letterSpacing = 0.5.sp
+    ),
+
     h1 =TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = font_standard,
         fontWeight = FontWeight.Medium,
         fontSize = 20.sp,
-    )
+        lineHeight = 25.sp,
+
+        )
 
 )
