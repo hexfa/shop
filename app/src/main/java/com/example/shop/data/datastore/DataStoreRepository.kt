4083ef4718ddf0a225257a006cbe34ecc1cd8105
@hexfa <amir.dehdarian2009@gmail.com>
2025-04-29 22:43:35 -0800

    feat: add suspend getInt to DataStoreRepository

--- app/src/main/java/com/example/shop/data/datastore/DataStoreRepository.kt
+++ app/src/main/java/com/example/shop/data/datastore/DataStoreRepository.kt
@@ -4,5 +4,6 @@ interface DataStoreRepository {
     suspend fun putString(key:String,value:String)
     suspend fun putInt(key:String,value:String)
     suspend fun getString(key:String):String
+    suspend fun getInt(key:String):Int
 
 }
