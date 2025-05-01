a7f2ecd90e559321644dab588fabcca61ba4701a
@hexfa <amir.dehdarian2009@gmail.com>
2025-04-30 22:10:57 -0800

    feat: add AppConfig composable with DataStoreViewModel injection

---
+++ app/src/main/java/com/example/shop/ui/components/AppConfig.kt
@@ -0,0 +1,12 @@
+package com.example.shop.ui.components
+
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.shop.viewmodels.DataStoreViewModel
+
+@Composable
+fun AppConfig(
+    dataStore: DataStoreViewModel = hiltViewModel()
+){
+
+}
