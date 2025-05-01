bfbc47c4e54e90ca3c3ab37dd0406a3a7f4bc1b1
@hexfa <amir.dehdarian2009@gmail.com>
2025-04-30 21:58:19 -0800

    feat: inject DataStoreRepository into DataStoreViewModel
    - Add `@Inject` constructor to `DataStoreViewModel` accepting `DataStoreRepository`
    - Enables Hilt-provided repository dependency for DataStore operations in the ViewModel

--- app/src/main/java/com/example/shop/viewmodels/DataStoreViewModel.kt
+++ app/src/main/java/com/example/shop/viewmodels/DataStoreViewModel.kt
@@ -1,7 +1,11 @@
 package com.example.shop.viewmodels
 
+import com.example.shop.data.datastore.DataStoreRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class DataStoreViewModel {
+class DataStoreViewModel @Inject constructor(
+    private val repository: DataStoreRepository
+) {
 }
