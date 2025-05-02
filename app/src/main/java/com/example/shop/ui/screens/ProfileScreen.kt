5af9bc21ee6563828574e62cef3adfe4d4e46e7c
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-01 17:43:17 -0800

    feat: persist Persian language selection and restart app on button click

--- app/src/main/java/com/example/shop/ui/screens/ProfileScreen.kt
+++ app/src/main/java/com/example/shop/ui/screens/ProfileScreen.kt
@@ -1,5 +1,8 @@
 package com.example.shop.ui.screens
 
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -10,19 +13,36 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import com.example.shop.MainActivity
+import com.example.shop.utils.Constants.PERSIAN
+import com.example.shop.viewmodels.DataStoreViewModel
 
+@SuppressLint("ContextCastToActivity")
 @Composable
-fun ProfileScreen(navController: NavHostController){
+fun ProfileScreen(navController: NavHostController,
+                  dataStore:DataStoreViewModel= hiltViewModel()
+){
     Column (
         modifier = Modifier.fillMaxSize()
             .background(Color.Blue),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ){
+
+        val activity= LocalContext.current as Activity
+
         Text("profile")
 
         Button(onClick = {
+
+            dataStore.saveUserLanguage(PERSIAN)
+            activity.apply {
+                finish()
+                startActivity(Intent(activity,MainActivity::class.java))
+            }
 
         }) {
             Text("fa")
