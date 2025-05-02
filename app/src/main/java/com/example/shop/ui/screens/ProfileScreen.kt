53b5595e425e8e6bf69dd5a4ee61e7e8e984bf91
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-01 17:35:17 -0800

    feat: add language toggle buttons to ProfileScreen

--- app/src/main/java/com/example/shop/ui/screens/ProfileScreen.kt
+++ app/src/main/java/com/example/shop/ui/screens/ProfileScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -20,5 +21,17 @@ fun ProfileScreen(navController: NavHostController){
         verticalArrangement = Arrangement.Center
     ){
         Text("profile")
+
+        Button(onClick = {
+
+        }) {
+            Text("fa")
+        }
+
+        Button(onClick = {
+
+        }) {
+            Text("en")
+        }
     }
 }
