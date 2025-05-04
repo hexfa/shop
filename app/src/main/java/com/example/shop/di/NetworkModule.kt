3500fc67a0b737147b49674ff3a5c44cdc7760ad
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-03 22:54:08 -0800

    feat: add network dependencies provision with Hilt

--- app/src/main/java/com/example/shop/di/NetworkModule.kt
+++ app/src/main/java/com/example/shop/di/NetworkModule.kt
@@ -4,23 +4,28 @@ import com.example.shop.data.remote.ApiInterface
 import com.example.shop.utils.Constants.BASE_URL
 import com.example.shop.utils.Constants.TIMEOUT_IN_SECOND
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
 
+    @Provides
+    @Singleton
     fun provideOkHttp():OkHttpClient=OkHttpClient.Builder()
         .connectTimeout(TIMEOUT_IN_SECOND,TimeUnit.SECONDS)
         .readTimeout(TIMEOUT_IN_SECOND,TimeUnit.SECONDS)
         .writeTimeout(TIMEOUT_IN_SECOND,TimeUnit.SECONDS)
         .build()
-
+    @Provides
+    @Singleton
     fun provideRetrofit(okHttpClient: OkHttpClient):Retrofit=
         Retrofit.Builder()
             .baseUrl(BASE_URL)
@@ -28,6 +33,8 @@ object NetworkModule {
             .client(okHttpClient)
             .build()
 
+    @Provides
+    @Singleton
     fun provideApiService(retrofit: Retrofit):ApiInterface=
         retrofit.create(ApiInterface::class.java)
 }
