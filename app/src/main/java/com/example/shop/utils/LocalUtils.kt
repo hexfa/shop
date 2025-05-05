6d4ae194e8fd73459e16e1a9d866a2b6d2611a36
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-04 22:24:54 -0800

    feat:refactor setLocale in LocalUtils

--- app/src/main/java/com/example/shop/utils/LocalUtils.kt
+++ app/src/main/java/com/example/shop/utils/LocalUtils.kt
@@ -7,10 +7,6 @@ import java.util.Locale
 object LocalUtils {
 
     fun setLocale(context: Context,language:String)= updateResource(context,language)
-
-
-
-
     private fun updateResource(context: Context,language:String){
         context.resources.apply {
             val local=Locale(language)
