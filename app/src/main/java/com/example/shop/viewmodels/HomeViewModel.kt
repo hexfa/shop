08e68c5e209c27a142c49af4977f1b8ff409e47a
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-05 22:51:56 -0800

    feat: add getSlider coroutine in HomeViewModel

--- app/src/main/java/com/example/shop/viewmodels/HomeViewModel.kt
+++ app/src/main/java/com/example/shop/viewmodels/HomeViewModel.kt
@@ -1,10 +1,20 @@
 package com.example.shop.viewmodels
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.shop.repository.HomeRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(private val repository: HomeRepository):ViewModel(){
+
+    suspend fun getSlider(){
+
+        viewModelScope.launch {
+            repository.getSlider()
+
+        }
+    }
 }
