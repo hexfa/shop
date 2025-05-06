6ce4caaa7c8efe0820ee8643d773763610e6873a
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-05 22:54:18 -0800

    feat: add slider StateFlow with initial Loading state

--- app/src/main/java/com/example/shop/viewmodels/HomeViewModel.kt
+++ app/src/main/java/com/example/shop/viewmodels/HomeViewModel.kt
@@ -2,14 +2,19 @@ package com.example.shop.viewmodels
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.shop.data.model.home.Slider
+import com.example.shop.data.remote.NetworkResult
 import com.example.shop.repository.HomeRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(private val repository: HomeRepository):ViewModel(){
 
+
+    val slider= MutableStateFlow<NetworkResult<List<Slider>>>(NetworkResult.Loading())
     suspend fun getSlider(){
 
         viewModelScope.launch {
