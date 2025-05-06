c39ffaa5a11d9ad13d9ed7c3a4dd4b7d834fa991
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-05 18:21:29 -0800

    refactor: wrap safeApiCall in withContext and return result properly

--- app/src/main/java/com/example/shop/data/remote/BaseApiResponse.kt
+++ app/src/main/java/com/example/shop/data/remote/BaseApiResponse.kt
@@ -1,25 +1,31 @@
 package com.example.shop.data.remote
 
 import com.example.shop.data.model.ResponseResult
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import retrofit2.Response
 
 abstract class BaseApiResponse {
     suspend fun <T> safeApiCall(apiCall:suspend()-> Response<ResponseResult<T>>):NetworkResult<T> =
-    try{
 
-        val response=apiCall()
+        withContext(Dispatchers.IO){
+            try{
 
-        if (response.isSuccessful){
-            val body=response.body()
-            body?.let {
-                return NetworkResult.Success(body.message,body.data)
+                val response=apiCall()
+
+                if (response.isSuccessful){
+                    val body=response.body()
+                    body?.let {
+                        return NetworkResult.Success(body.message,body.data)
+                    }
+                }
+                return error("code :${response.code()}  message:${response.message()}")
+            }catch (e: Exception){
+                return error(e.message?:e.toString())
+
             }
         }
-        return error("code :${response.code()}  message:${response.message()}")
-    }catch (e: Exception){
-        return error(e.message?:e.toString())
 
-    }
 
     private fun <T> error(errorMessage:String):NetworkResult<T> =
         NetworkResult.Error("Api call failed: $errorMessage")
