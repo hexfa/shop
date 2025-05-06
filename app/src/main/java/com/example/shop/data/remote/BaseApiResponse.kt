53f3bbd341960a042a26a68ae88cfc399d9b1729
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-05 18:09:18 -0800

    feat: add generic safeApiCall to BaseApiResponse

--- app/src/main/java/com/example/shop/data/remote/BaseApiResponse.kt
+++ app/src/main/java/com/example/shop/data/remote/BaseApiResponse.kt
@@ -1,5 +1,22 @@
 package com.example.shop.data.remote
 
+import com.example.shop.data.model.ResponseResult
+import retrofit2.Response
+
 abstract class BaseApiResponse {
-  //  suspend fun <T> safeApiCall(apiCall:suspend()->Response<ResponseResult<T>>)
+    suspend fun <T> safeApiCall(apiCall:suspend()-> Response<ResponseResult<T>>):NetworkResult<T> =
+    try{
+
+        val response=apiCall()
+
+        if (response.isSuccessful){
+            val body=response.body()
+            body?.let {
+                return NetworkResult.Success(body.message,body.data)
+            }
+        }
+        return
+    }catch (){
+
+    }
 }
