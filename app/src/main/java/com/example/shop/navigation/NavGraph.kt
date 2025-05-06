1c657eb5d2d0f51c734563161832aad41b69ffac
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-05 21:23:10 -0800

    refactor: change package home add to home package

--- app/src/main/java/com/example/shop/navigation/NavGraph.kt
+++ app/src/main/java/com/example/shop/navigation/NavGraph.kt
@@ -5,7 +5,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.example.shop.ui.screens.BasketScreen
-import com.example.shop.ui.screens.HomeScreen
+import com.example.shop.ui.screens.home.HomeScreen
 import com.example.shop.ui.screens.ProfileScreen
 import com.example.shop.ui.screens.SplashScreen
 
