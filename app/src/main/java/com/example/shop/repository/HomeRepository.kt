2b7affbef968d0f077cc7fda66865c5e8dc458b6
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-05 22:47:54 -0800

    feat: expose getSlider repository method with safeApiCall

--- app/src/main/java/com/example/shop/repository/HomeRepository.kt
+++ app/src/main/java/com/example/shop/repository/HomeRepository.kt
@@ -1,9 +1,16 @@
 package com.example.shop.repository
 
+import com.example.shop.data.model.home.Slider
 import com.example.shop.data.remote.BaseApiResponse
 import com.example.shop.data.remote.HomeApiInterface
+import com.example.shop.data.remote.NetworkResult
 import javax.inject.Inject
 
 
 class HomeRepository @Inject constructor(private val api:HomeApiInterface):BaseApiResponse() {
+
+    suspend fun getSlider():NetworkResult<List<Slider>> =
+        safeApiCall {
+            api.getSlider()
+        }
 }
