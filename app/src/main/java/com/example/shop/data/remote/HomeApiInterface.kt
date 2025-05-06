971a6da52259cf773d8fe170827979584cbe0d93
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-05 22:42:46 -0800

    feat: add Slider data model
    - Define `Slider` data class with _id, image, url, category, and priority fields
    - Used for parsing slider list in getSlider API response

--- app/src/main/java/com/example/shop/data/remote/HomeApiInterface.kt
+++ app/src/main/java/com/example/shop/data/remote/HomeApiInterface.kt
@@ -1,6 +1,7 @@
 package com.example.shop.data.remote
 
 import com.example.shop.data.model.ResponseResult
+import com.example.shop.data.model.home.Slider
 import retrofit2.Response
 import retrofit2.http.GET
 
