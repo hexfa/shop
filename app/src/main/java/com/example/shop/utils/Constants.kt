d9f3d4bf1a008558c0139f6f52f5d2ec4350c87b
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-06 22:46:05 -0800

    feat: Add apikey to Constants

--- app/src/main/java/com/example/shop/utils/Constants.kt
+++ app/src/main/java/com/example/shop/utils/Constants.kt
@@ -6,6 +6,8 @@ object Constants {
     const val DATASTORE_NAME="SHOP_DATA_STORE"
     const val TIMEOUT_IN_SECOND:Long=60
     const val BASE_URL="https://truelearn-digikala.iran.liara.run/api/"
+    const val API_KEY="EB43556E671B925B9C98E74643BCA"
+
     var USER_LANGUAGE="USER_LANGUAGE"
 
 }
