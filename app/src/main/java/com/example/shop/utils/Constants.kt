9441c8da7ced408cb3b151f558f1a25ad2ef48e8
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-06 23:26:53 -0800

    feat: change api_key in Constants

--- app/src/main/java/com/example/shop/utils/Constants.kt
+++ app/src/main/java/com/example/shop/utils/Constants.kt
@@ -1,12 +1,14 @@
 package com.example.shop.utils
 
+import com.google.gson.internal.GsonBuildConfig
+
 object Constants {
     const val ENGLISH="en"
     const val PERSIAN="fa"
     const val DATASTORE_NAME="SHOP_DATA_STORE"
     const val TIMEOUT_IN_SECOND:Long=60
     const val BASE_URL="https://truelearn-digikala.iran.liara.run/api/"
-    const val API_KEY="EB43556E671B925B9C98E74643BCA"
+    const val API_KEY=GsonBuildConfig.VERSION
 
     var USER_LANGUAGE="USER_LANGUAGE"
 
