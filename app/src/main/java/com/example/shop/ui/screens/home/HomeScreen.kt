76b829938507f7fe493af2a618ff10778db8fdc6
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-06 23:33:25 -0800

    feat: inject HomeViewModel to Home with HiltViewModel

--- app/src/main/java/com/example/shop/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/shop/ui/screens/home/HomeScreen.kt
@@ -1,7 +1,9 @@
 package com.example.shop.ui.screens.home
 
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import com.example.shop.viewmodels.HomeViewModel
 
 @Composable
 fun HomeScreen(navController: NavHostController){
@@ -10,6 +12,8 @@ fun HomeScreen(navController: NavHostController){
 }
 
 @Composable
-fun Home(navController: NavHostController){
+fun Home(navController: NavHostController,
+         viewModel: HomeViewModel = hiltViewModel()
+){
 
 }
