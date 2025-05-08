88d12944122fda2de58a01bd0f94ebd45982b053
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-07 17:54:34 -0800

    feat(ui): add full-screen white background to Home screen

--- app/src/main/java/com/example/shop/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/shop/ui/screens/home/HomeScreen.kt
@@ -1,6 +1,11 @@
 package com.example.shop.ui.screens.home
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.example.shop.viewmodels.HomeViewModel
@@ -15,5 +20,10 @@ fun HomeScreen(navController: NavHostController){
 fun Home(navController: NavHostController,
          viewModel: HomeViewModel = hiltViewModel()
 ){
+    Column (modifier =
+    Modifier
+        .background(color = Color.White)
+        .fillMaxSize()
+    ){  }
 
 }
