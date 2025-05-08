6ea22b78086452794c423e40de9d1dff959b7498
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-07 18:15:44 -0800

    refactor(ui): inject HomeViewModel directly into TopSlider composable using Hilt

--- app/src/main/java/com/example/shop/ui/screens/home/TopSlider.kt
+++ app/src/main/java/com/example/shop/ui/screens/home/TopSlider.kt
@@ -1,8 +1,10 @@
 package com.example.shop.ui.screens.home
 
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.shop.viewmodels.HomeViewModel
 
 @Composable
-fun TopSlider(){
+fun TopSlider(viewModel: HomeViewModel= hiltViewModel()){
 
 }
