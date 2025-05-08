472db2fa66b51e89ea62d4f8bce3aa676beeedca
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-07 18:11:50 -0800

    feat(viewmodel): fetch slider data on Home screen launch using LaunchedEffect

--- app/src/main/java/com/example/shop/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/shop/ui/screens/home/HomeScreen.kt
@@ -54,7 +54,7 @@ fun Home(navController: NavHostController,
             ){
 
                 LaunchedEffect(true) {
-
+                    viewModel.getSlider();
                 }
             }
         }
