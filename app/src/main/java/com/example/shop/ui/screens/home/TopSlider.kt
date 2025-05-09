fefaa5f3f2ae2cd7747a72c32eefe09a1895dd23
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-08 22:19:22 -0800

    feat(ui): create fixed-height white Column for top banner section

--- app/src/main/java/com/example/shop/ui/screens/home/TopSlider.kt
+++ app/src/main/java/com/example/shop/ui/screens/home/TopSlider.kt
@@ -1,11 +1,18 @@
 package com.example.shop.ui.screens.home
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.shop.data.model.home.Slider
 import com.example.shop.data.remote.NetworkResult
@@ -43,6 +50,12 @@ fun TopSlider(viewModel: HomeViewModel= hiltViewModel()) {
 
 
     }
+
+    Column (modifier = Modifier
+        .fillMaxWidth()
+        .height(200.dp)
+        .background(Color.White)
+    ){  }
 }
 
 
