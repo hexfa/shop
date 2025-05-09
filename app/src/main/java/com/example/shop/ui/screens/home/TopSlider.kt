84d69379950ec1ec4ff5d17848be0784d912f646
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-08 21:59:20 -0800

    chore(ui): add loading state using remember for TopSlider

--- app/src/main/java/com/example/shop/ui/screens/home/TopSlider.kt
+++ app/src/main/java/com/example/shop/ui/screens/home/TopSlider.kt
@@ -2,6 +2,7 @@ package com.example.shop.ui.screens.home
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.shop.data.remote.NetworkResult
 import com.example.shop.viewmodels.HomeViewModel
@@ -9,6 +10,9 @@ import kotlinx.coroutines.flow.collectLatest
 
 @Composable
 fun TopSlider(viewModel: HomeViewModel= hiltViewModel()){
+
+    var loading by remember {  }
+
     LaunchedEffect(true) {
         viewModel.slider.collectLatest {result->
             when (result){
