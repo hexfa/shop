c69c5b88e8a8678dd67c999069ffff2f91d40543
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-08 21:53:21 -0800

    chore(ui): prepare LaunchedEffect block in TopSlider composable

--- app/src/main/java/com/example/shop/ui/screens/home/TopSlider.kt
+++ app/src/main/java/com/example/shop/ui/screens/home/TopSlider.kt
@@ -1,10 +1,14 @@
 package com.example.shop.ui.screens.home
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.shop.viewmodels.HomeViewModel
 
 @Composable
 fun TopSlider(viewModel: HomeViewModel= hiltViewModel()){
+    LaunchedEffect(true) {
+
+    }
 
 }
