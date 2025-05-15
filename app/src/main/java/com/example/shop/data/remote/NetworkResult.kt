805d9b83ef9dd6169c73ff29e1b91e00bfb4a1af
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-14 22:31:48 -0800

    chore: centralize network timeout constants in NetworkModule

--- app/src/main/java/com/example/shop/data/remote/NetworkResult.kt
+++ app/src/main/java/com/example/shop/data/remote/NetworkResult.kt
@@ -1,15 +1,15 @@
 package com.example.shop.data.remote
 
 sealed class NetworkResult<T>(
-    val message:String?=null,
-    val data:T?=null,
+    val message: String? = null,
+    val data: T? = null,
 
 
-    ){
-    class Success<T>(message: String?,data: T?):NetworkResult<T>(message,data)
+    ) {
+    class Success<T>(message: String?, data: T?) : NetworkResult<T>(message, data)
 
-    class Loading<T>():NetworkResult<T>()
+    class Loading<T>() : NetworkResult<T>()
 
-    class Error<T>(message: String?,data: T?=null):NetworkResult<T>(message,data)
+    class Error<T>(message: String?, data: T? = null) : NetworkResult<T>(message, data)
 
 }
