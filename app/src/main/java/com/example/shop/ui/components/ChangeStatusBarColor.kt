a47c40200d6a4333f7eb443b97a2b4a4b3685416
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-14 22:36:22 -0800

    feat: create Product data model with id, name, price, inventory, and category

--- app/src/main/java/com/example/shop/ui/components/ChangeStatusBarColor.kt
+++ app/src/main/java/com/example/shop/ui/components/ChangeStatusBarColor.kt
@@ -11,23 +11,25 @@ import com.example.shop.ui.theme.Purple80
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 @Composable
-fun ChangeStatusBarColor(navController: NavHostController){
+fun ChangeStatusBarColor(navController: NavHostController) {
 
     val navBackStackEntry by navController.currentBackStackEntryAsState()
     val systemUiController = rememberSystemUiController()
 
-    when(navBackStackEntry?.destination?.route){
-        Screen.Splash.route->{
+    when (navBackStackEntry?.destination?.route) {
+        Screen.Splash.route -> {
 
             SideEffect {
                 systemUiController.setStatusBarColor(
                     color = Purple80
                 )
             }
-        }else->
-        systemUiController.setStatusBarColor(
-            color = Color.White
-        )
+        }
+
+        else ->
+            systemUiController.setStatusBarColor(
+                color = Color.White
+            )
 
     }
 }
