a2c3d094a80e59314a633224743f372119fb425a
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-14 22:35:31 -0800

    feat: add Retrofit API interface for product and user endpoints

--- app/src/main/java/com/example/shop/navigation/Screen.kt
+++ app/src/main/java/com/example/shop/navigation/Screen.kt
@@ -1,14 +1,14 @@
 package com.example.shop.navigation
 
-sealed class Screen(val route :String) {
-    object Splash: Screen("splash_screen")
-    object Home: Screen("home_screen")
-    object Basket: Screen("basket_screen")
-    object Profile: Screen("profile_screen")
-    object Category: Screen("category_screen")
+sealed class Screen(val route: String) {
+    object Splash : Screen("splash_screen")
+    object Home : Screen("home_screen")
+    object Basket : Screen("basket_screen")
+    object Profile : Screen("profile_screen")
+    object Category : Screen("category_screen")
 
-    fun withArgs(vararg args:Any):String{
-        return  buildString {
+    fun withArgs(vararg args: Any): String {
+        return buildString {
             append(route)
             args.forEach {
                 append("/$it")
