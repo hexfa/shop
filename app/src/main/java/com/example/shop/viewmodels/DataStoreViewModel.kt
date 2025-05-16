84b0d0a4c05858bec851ac3912a8f5336deb1280
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-15 22:54:06 -0800

    fix: update getProducts to emit loading, success, and error states properly

--- app/src/main/java/com/example/shop/viewmodels/DataStoreViewModel.kt
+++ app/src/main/java/com/example/shop/viewmodels/DataStoreViewModel.kt
@@ -14,6 +14,8 @@ class DataStoreViewModel @Inject constructor(
     private val repository: DataStoreRepository
 ) : ViewModel() {
 
+
+
     companion object {
         const val USER_LANGUAGE_KEY = "USER_LANGUAGE_KEY"
     }
