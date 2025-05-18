5318de0dd6f1c7470dfe82c203af17eb771936be
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-17 22:25:28 -0800

    refactor: change name of some items

--- app/src/main/java/com/example/shop/ui/screens/SplashScreen.kt
+++ app/src/main/java/com/example/shop/ui/screens/SplashScreen.kt
@@ -1,21 +1,19 @@
 package com.example.shop.ui.screens
 
-//noinspection UsingMaterialAndMaterial3Libraries
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+//noinspection UsingMaterialAndMaterial3Libraries
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.example.shop.R
@@ -27,37 +25,27 @@ import kotlinx.coroutines.delay
 
 @Composable
 fun SplashScreen(navController: NavHostController) {
-    val SPLASH_DELAY_MS = 2000L
-    val HOME_ROUTE = Screen.Home.route
-    SplashContent()
-    LaunchedEffect(Unit) {
-        waitAndNavigate(SPLASH_DELAY_MS, navController, HOME_ROUTE)
+
+    Splash()
+    LaunchedEffect(true) {
+        delay(2000)
+        navController.navigate(Screen.Home.route)
     }
 }
 
-private suspend fun waitAndNavigate(
-    SPLASH_DELAY_MS: Long,
-    navController: NavHostController,
-    HOME_ROUTE: String
-) {
-    delay(SPLASH_DELAY_MS)
-    navController.navigate(HOME_ROUTE)
-}
-
-@Preview(showBackground = true)
 @Composable
-fun SplashContent() {
-    val fullSize = Modifier.fillMaxSize()
-    val CONTENT_PADDING = 20.dp
+fun Splash() {
     Box(
-        modifier = fullSize.background(MaterialTheme.colors.splashBg),
+        modifier = Modifier
+            .background(MaterialTheme.colors.splashBg)
+            .fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
 
 
         Image(
             painter = painterResource(R.drawable.digi_logo),
-            contentDescription = "Shop logo",
+            contentDescription = null,
             modifier = Modifier.size(250.dp)
         )
     }
@@ -66,7 +54,9 @@ fun SplashContent() {
 
 
     Box(
-        modifier = Modifier.fillMaxHeight().padding(CONTENT_PADDING * 5),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(100.dp),
         contentAlignment = Alignment.BottomCenter
 
 
@@ -79,16 +69,19 @@ fun SplashContent() {
 
     }
 
+
+
+
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .padding(CONTENT_PADDING),
+            .padding(20.dp),
         contentAlignment = Alignment.BottomCenter
 
 
     ) {
 
-        Loading3Dots(false)
+        Loading3Dots(false);
 
     }
 }
