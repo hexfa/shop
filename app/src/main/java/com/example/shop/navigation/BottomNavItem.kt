939e3795fccc1ca59ff280bd07f79e6abdaf6114
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-25 23:02:20 -0800

    feat:clean code

--- app/src/main/java/com/example/shop/navigation/BottomNavItem.kt
+++ app/src/main/java/com/example/shop/navigation/BottomNavItem.kt
@@ -4,8 +4,11 @@ import androidx.compose.ui.graphics.painter.Painter
 
 data class BottomNavItem(
     val name: String,
+
     val route: String,
+
     val selectedIcon: Painter,
+
     val deSelectedIcon: Painter
 
 )
