cc1958aeef0884f1d4030643764a11198f032b64
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-25 22:59:35 -0800

    feat:clean code

--- app/src/main/java/com/example/shop/data/model/home/Slider.kt
+++ app/src/main/java/com/example/shop/data/model/home/Slider.kt
@@ -1,9 +1,14 @@
 package com.example.shop.data.model.home
 
 data class Slider(
+
     val _id:String,
+
     val image:String,
+
     val url:String,
+
     val category:String,
+
     val priority:Int
 )
