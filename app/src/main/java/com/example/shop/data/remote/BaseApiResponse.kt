91d76e08180411adcde34b2fe82230c01e614d4f
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-25 23:00:41 -0800

    feat:clean code

--- app/src/main/java/com/example/shop/data/remote/BaseApiResponse.kt
+++ app/src/main/java/com/example/shop/data/remote/BaseApiResponse.kt
@@ -6,15 +6,18 @@ import kotlinx.coroutines.withContext
 import retrofit2.Response
 
 open class BaseApiResponse {
+
     suspend fun <T> safeApiCall(apiCall: suspend () -> Response<ResponseResult<T>>): NetworkResult<T> =
 
 
         withContext(Dispatchers.IO) {
+
             try {
 
                 val response = apiCall()
 
                 if (response.isSuccessful) {
+
                     val body = response.body()
                     body?.let {
                         return@withContext NetworkResult.Success(body.message, body.data)
