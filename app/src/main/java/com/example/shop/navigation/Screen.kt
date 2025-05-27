c5501999642bf2938a9a8077304749d94d0c715b
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-26 23:22:17 -0800

    feat:clean code

--- app/src/main/java/com/example/shop/navigation/Screen.kt
+++ app/src/main/java/com/example/shop/navigation/Screen.kt
@@ -1,6 +1,7 @@
 package com.example.shop.navigation
 
 sealed class Screen(val route: String) {
+
     object Splash : Screen("splash_screen")
     object Home : Screen("home_screen")
     object Basket : Screen("basket_screen")
