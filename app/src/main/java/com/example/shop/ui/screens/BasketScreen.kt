502997c03b29dbd2dda9a26629c5091a5f54cdcb
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-31 23:21:41 -0800

    feat:clean code

--- app/src/main/java/com/example/shop/ui/screens/BasketScreen.kt
+++ app/src/main/java/com/example/shop/ui/screens/BasketScreen.kt
@@ -64,6 +64,7 @@ fun BasketDark() {
             .background(Color.DarkGray),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
+
     ) {
         Text("BasketScreen", color = MaterialTheme.colors.selectedBottomBar)
 
