32c34e1249fc9ef1d82ab1f9fe72177a8b508bc5
@hexfa <amir.dehdarian2009@gmail.com>
2025-06-02 21:58:53 -0800

    feat:clean code

--- app/src/main/java/com/example/shop/data/model/cart.kt
+++ app/src/main/java/com/example/shop/data/model/cart.kt
@@ -277,6 +277,7 @@ class InMemoryCartRepository : CartRepository {
                             modifier = Modifier
                                 .fillMaxWidth()
                                 .padding(16.dp),
+
                             horizontalArrangement = Arrangement.SpaceBetween
                         ) {
                             Text("Total:")
