ea775cc9ef54d443e0b3eb588451e09ee332059c
@hexfa <amir.dehdarian2009@gmail.com>
2025-06-05 23:13:33 -0800

    feat:clean code

--- app/src/main/java/com/example/shop/ui/components/ChangeStatusBarColor.kt
+++ app/src/main/java/com/example/shop/ui/components/ChangeStatusBarColor.kt
@@ -27,6 +27,7 @@ fun ChangeStatusBarColor(navController: NavHostController) {
 
             SideEffect {
                 systemUiController.setStatusBarColor(
+
                     color = Purple80
                 )
             }
