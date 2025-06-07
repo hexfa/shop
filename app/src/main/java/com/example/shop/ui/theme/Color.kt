832612523285bd7e9b1dc0b34b458543f2104e13
@hexfa <amir.dehdarian2009@gmail.com>
2025-06-06 23:11:55 -0800

    feat:clean code

--- app/src/main/java/com/example/shop/ui/theme/Color.kt
+++ app/src/main/java/com/example/shop/ui/theme/Color.kt
@@ -21,6 +21,7 @@ val Colors.selectedBottomBar : Color
    @Composable
    get()=if(isLight) Color(0xFF43474C) else Color(0xFFCFD4DA)
 
+
 val Colors.unSelectedBottomBar : Color
    @Composable
    get()=if(isLight) Color(0xFFA4A1A1) else Color(0xFF575A5E)
