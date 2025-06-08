7d3f272a650f25fc5087621da61498dfb80a35f7
@hexfa <amir.dehdarian2009@gmail.com>
2025-06-07 22:35:44 -0800

    feat:clean code

--- app/src/main/java/com/example/shop/MainActivity.kt
+++ app/src/main/java/com/example/shop/MainActivity.kt
@@ -94,12 +94,13 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
-        
+
         super.onCreate(savedInstanceState)
 
         enableEdgeToEdge()
 
         logAppConfig()
+
 
 
         setContent {
