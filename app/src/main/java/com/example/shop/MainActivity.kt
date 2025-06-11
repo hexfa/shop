3ca7c694d3f7bfa8087f42fac296281d75eba7d3
@hexfa <amir.dehdarian2009@gmail.com>
2025-06-10 23:52:05 -0800

    feat:clean code

--- app/src/main/java/com/example/shop/MainActivity.kt
+++ app/src/main/java/com/example/shop/MainActivity.kt
@@ -82,12 +82,13 @@ class MainActivity : ComponentActivity() {
 
     @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 
-    
+
     @Composable
 
     fun MainContent() {
 
 
+        
         val navController = rememberNavController()
 
 
