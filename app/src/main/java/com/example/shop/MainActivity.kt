fa78206e39f84e3cbdfb968a7ec0763b8fc1e89b
@hexfa <amir.dehdarian2009@gmail.com>
2025-06-12 21:34:50 -0800

    feat:clean code

--- app/src/main/java/com/example/shop/MainActivity.kt
+++ app/src/main/java/com/example/shop/MainActivity.kt
@@ -126,7 +126,6 @@ class MainActivity : ComponentActivity() {
 
                 CompositionLocalProvider(LocalLayoutDirection provides getLayoutDirection()) {
 
-
                     MainContent()
                 }
 
