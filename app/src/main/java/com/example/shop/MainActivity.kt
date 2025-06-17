dff0b28aebfbf7f3c2b8ad76e9040309b9d163a5
@hexfa <amir.dehdarian2009@gmail.com>
2025-06-16 23:22:35 -0800

    feat:clean code

--- app/src/main/java/com/example/shop/MainActivity.kt
+++ app/src/main/java/com/example/shop/MainActivity.kt
@@ -49,7 +49,6 @@ class MainActivity : ComponentActivity() {
 
         Log.d("MainActivity", "com.example.shop.data.model.User language: $USER_LANGUAGE, LayoutDirection: ${getLayoutDirection()}")
     }
-
     @Composable
 
 
