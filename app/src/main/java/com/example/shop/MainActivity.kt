89de1ac7d0903cc1d4d15c37485eb7889bf1d620
@hexfa <amir.dehdarian2009@gmail.com>
2025-06-16 23:21:52 -0800

    feat:clean code

--- app/src/main/java/com/example/shop/MainActivity.kt
+++ app/src/main/java/com/example/shop/MainActivity.kt
@@ -45,7 +45,6 @@ class MainActivity : ComponentActivity() {
         return if (USER_LANGUAGE == ENGLISH) {
             LayoutDirection.Ltr
         } else {
-
             LayoutDirection.Rtl
 
         }
