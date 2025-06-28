0092452dc12dc52fc1a52b1b7a85688b9fb1ff95
@hexfa <amir.dehdarian2009@gmail.com>
2025-06-27 23:03:27 -0800

    feat:clean code

--- app/src/main/java/com/example/shop/MainActivity.kt
+++ app/src/main/java/com/example/shop/MainActivity.kt
@@ -49,7 +49,6 @@ class MainActivity : ComponentActivity() {
 
 
     private fun logAppConfig() {
-        
         Log.d(
             "MainActivity",
             "com.example.shop.data.model.User language: $USER_LANGUAGE, LayoutDirection: ${getLayoutDirection()}"
