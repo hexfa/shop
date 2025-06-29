0c7faac2811c255c62f1f7dab47d88dc957134f0
@hexfa <amir.dehdarian2009@gmail.com>
2025-06-28 23:14:15 -0800

    feat:clean code

--- app/src/main/java/com/example/shop/data/datastore/DataStoreRepositoryImp.kt
+++ app/src/main/java/com/example/shop/data/datastore/DataStoreRepositoryImp.kt
@@ -42,8 +42,9 @@ class DataStoreRepositoryImp @Inject constructor(
 
 
         } catch (e: Exception) {
-            
+
             e.printStackTrace()
+            
             return null
         }
     }
