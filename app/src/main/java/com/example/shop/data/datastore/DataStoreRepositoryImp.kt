bf7346631d47b66307a8c2d8eae88f502ac34792
@hexfa <amir.dehdarian2009@gmail.com>
2025-06-28 23:15:36 -0800

    feat:clean code

--- app/src/main/java/com/example/shop/data/datastore/DataStoreRepositoryImp.kt
+++ app/src/main/java/com/example/shop/data/datastore/DataStoreRepositoryImp.kt
@@ -24,15 +24,14 @@ class DataStoreRepositoryImp @Inject constructor(
         }
     }
 
-
-
+    
     override suspend fun putInt(key: String, value: Int) {
         val preferencesKey = intPreferencesKey(key)
         context.datastore.edit { preferences ->
             preferences[preferencesKey] = value
         }
     }
-    
+
     override suspend fun getString(key: String): String? {
         return try {
             val preferencesKey = stringPreferencesKey(key)
