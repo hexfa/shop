7eb555914dcc56d3f0ed200e7ce6de861862fb31
@hexfa <amir.dehdarian2009@gmail.com>
2025-07-05 21:40:39 -0800

    feat:clean code

--- app/src/main/java/com/example/shop/data/datastore/DataStoreRepositoryImp.kt
+++ app/src/main/java/com/example/shop/data/datastore/DataStoreRepositoryImp.kt
@@ -11,7 +11,6 @@ import com.example.shop.utils.Constants.DATASTORE_NAME
 import kotlinx.coroutines.flow.first
 import javax.inject.Inject
 
-
 private val Context.datastore: DataStore<Preferences> by preferencesDataStore(name = DATASTORE_NAME)
 class DataStoreRepositoryImp @Inject constructor(
     private val context: Context
