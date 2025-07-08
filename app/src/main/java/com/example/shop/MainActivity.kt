ad863cb9ab8c01bd7ee7256289c829275358f6ae
@hexfa <amir.dehdarian2009@gmail.com>
2025-07-07 17:38:57 -0800

    feat:clean code

--- app/src/main/java/com/example/shop/MainActivity.kt
+++ app/src/main/java/com/example/shop/MainActivity.kt
@@ -58,8 +58,7 @@ class MainActivity : ComponentActivity() {
 
     fun MainContent() {
         val navController = rememberNavController()
-
-
+        
         Scaffold(bottomBar = {
 
             MainBottomBar(navController)
