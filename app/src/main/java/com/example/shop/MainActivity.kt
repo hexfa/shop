73a69083f02065e8ceb6d282c3c0140eb7fb98ad
@hexfa <amir.dehdarian2009@gmail.com>
2025-07-07 17:36:20 -0800

    feat:clean code

--- app/src/main/java/com/example/shop/MainActivity.kt
+++ app/src/main/java/com/example/shop/MainActivity.kt
@@ -49,7 +49,6 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     fun MainBottomBar(navController: androidx.navigation.NavHostController) {
-        
         BottomNavigationBar(navController = navController, onItemClick = {
 
             navController.navigate(it.route)
