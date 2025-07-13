e35fa7760848bdd15dde6881bacf36e619f348a8
@hexfa <amir.dehdarian2009@gmail.com>
2025-07-12 22:56:17 -0800

    feat:update gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,9 +82,10 @@ dependencies {
 
     implementation(libs.rendering)
     implementation(libs.androidx.recyclerview)
-    
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
+
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
