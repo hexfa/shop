85099fc64ad2188cbcf498be28db1c0472767210
@hexfa <amir.dehdarian2009@gmail.com>
2025-07-13 22:37:47 -0800

    feat:update gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,7 +58,6 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
-
     implementation(libs.androidx.ui)
 
     implementation(libs.androidx.ui.graphics)
