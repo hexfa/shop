ca74ffa9eab74d68f10f48c0027f2314b02527ed
@hexfa <amir.dehdarian2009@gmail.com>
2025-07-13 22:37:57 -0800

    feat:update gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,7 +60,6 @@ dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
-
     implementation(libs.androidx.ui.tooling.preview)
 
     implementation(libs.androidx.material3)
