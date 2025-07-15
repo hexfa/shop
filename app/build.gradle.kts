722f9a6aecbbda5942987608402b5efa8bd29611
@hexfa <amir.dehdarian2009@gmail.com>
2025-07-14 23:13:27 -0800

    feat:update gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,7 +68,6 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.media3.effect)
     implementation(libs.rendering)
-
     implementation(libs.androidx.recyclerview)
 
     testImplementation(libs.junit)
