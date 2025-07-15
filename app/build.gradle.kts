bafd23bf1edf0bbcbdaabbe6f58530c3750f1df9
@hexfa <amir.dehdarian2009@gmail.com>
2025-07-14 23:18:40 -0800

    feat:update gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,7 +76,6 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-
     //retrofit
     implementation(libs.retrofit)
     implementation(libs.retrofit.converter.gson)
