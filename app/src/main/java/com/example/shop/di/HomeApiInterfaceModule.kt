cddb01064d2018173cdbeb0869687b66d0cbebb5
@hexfa <amir.dehdarian2009@gmail.com>
2025-07-16 23:32:02 -0800

    feat:fixed HomeApiInterfaceModule

--- app/src/main/java/com/example/shop/di/HomeApiInterfaceModule.kt
+++ app/src/main/java/com/example/shop/di/HomeApiInterfaceModule.kt
@@ -12,9 +12,10 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 object HomeApiInterfaceModule {
     @Provides
+    
     @Singleton
 
     fun provideHomeApiService(retrofit: Retrofit): HomeApiInterface =
-        
+
         retrofit.create(HomeApiInterface::class.java)
 }
