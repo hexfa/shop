4bdfefa47e2b9e64084f82df6f6e049b99381b44
@hexfa <amir.dehdarian2009@gmail.com>
2025-07-19 22:26:20 -0800

    feat:fixed NetworkModule

--- app/src/main/java/com/example/shop/di/NetworkModule.kt
+++ app/src/main/java/com/example/shop/di/NetworkModule.kt
@@ -36,8 +36,9 @@ object NetworkModule {
     @Singleton
 
     fun provideOkHttp(): OkHttpClient = OkHttpClient.Builder()
-        
+
         .connectTimeout(TIMEOUT_IN_SECOND, TimeUnit.SECONDS)
+        
         .readTimeout(TIMEOUT_IN_SECOND, TimeUnit.SECONDS)
         .writeTimeout(TIMEOUT_IN_SECOND, TimeUnit.SECONDS)
         .addInterceptor { chain ->
