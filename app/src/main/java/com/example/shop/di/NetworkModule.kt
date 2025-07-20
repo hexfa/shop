aa124ff8be3cd8252ff493814aee5994344e1e53
@hexfa <amir.dehdarian2009@gmail.com>
2025-07-19 22:26:14 -0800

    feat:fixed NetworkModule

--- app/src/main/java/com/example/shop/di/NetworkModule.kt
+++ app/src/main/java/com/example/shop/di/NetworkModule.kt
@@ -23,7 +23,7 @@ object NetworkModule {
     @Singleton
     internal fun interceptor(): HttpLoggingInterceptor {
 
-        
+
         val logging = HttpLoggingInterceptor()
 
         logging.setLevel(HttpLoggingInterceptor.Level.BODY)
@@ -36,6 +36,7 @@ object NetworkModule {
     @Singleton
 
     fun provideOkHttp(): OkHttpClient = OkHttpClient.Builder()
+        
         .connectTimeout(TIMEOUT_IN_SECOND, TimeUnit.SECONDS)
         .readTimeout(TIMEOUT_IN_SECOND, TimeUnit.SECONDS)
         .writeTimeout(TIMEOUT_IN_SECOND, TimeUnit.SECONDS)
