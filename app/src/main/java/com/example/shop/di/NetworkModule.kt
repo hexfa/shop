8f7c80f053198971a7cd1efe99b78b9fdcb9b558
@hexfa <amir.dehdarian2009@gmail.com>
2025-07-20 23:10:50 -0800

    feat:fixed readTimeout NetworkModule

--- app/src/main/java/com/example/shop/di/NetworkModule.kt
+++ app/src/main/java/com/example/shop/di/NetworkModule.kt
@@ -35,9 +35,7 @@ object NetworkModule {
 
     @Singleton
     fun provideOkHttp(): OkHttpClient = OkHttpClient.Builder()
-
         .connectTimeout(TIMEOUT_IN_SECOND, TimeUnit.SECONDS)
-
         .readTimeout(TIMEOUT_IN_SECOND, TimeUnit.SECONDS)
 
         .writeTimeout(TIMEOUT_IN_SECOND, TimeUnit.SECONDS)
