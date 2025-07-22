e03c71d36f62f8bc059b6cdb9dfc8bcd2d15785a
@hexfa <amir.dehdarian2009@gmail.com>
2025-07-21 22:43:11 -0800

    feat:update provideApiService in NetworkModule

--- app/src/main/java/com/example/shop/di/NetworkModule.kt
+++ app/src/main/java/com/example/shop/di/NetworkModule.kt
@@ -48,8 +48,9 @@ object NetworkModule {
         .addInterceptor(interceptor())
 
         .build()
-    
+
     @Provides
+
     @Singleton
     fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit =
         Retrofit.Builder()
