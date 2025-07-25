0f1cef7cbce2ae690de438b76b3ec629bd2181ad
@hexfa <amir.dehdarian2009@gmail.com>
2025-07-24 20:40:25 -0800

    feat:update gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,6 +9,7 @@ plugins {
 val apikeyPropertiesFile=rootProject.file("key.properties")
 val apikeyProperties =  Properties()
 apikeyProperties.load(FileInputStream(apikeyPropertiesFile))
+
 android {
     namespace = "com.example.shop"
     compileSdk = 35
