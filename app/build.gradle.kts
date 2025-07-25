a7d161ac2cdbc50aa2a58773856aad3e24e7694d
@hexfa <amir.dehdarian2009@gmail.com>
2025-07-24 20:39:41 -0800

    feat:update gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,9 @@ android {
         }
     }
     compileOptions {
-        
+
         sourceCompatibility = JavaVersion.VERSION_11
+        
         targetCompatibility = JavaVersion.VERSION_11
     }
 
