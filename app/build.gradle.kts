5c173c341df47167963a19eeb633d1139b257ced
@hexfa <amir.dehdarian2009@gmail.com>
2025-07-26 22:17:29 -0800

    feat:update gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,9 @@ android {
         release {
 
             isMinifyEnabled = false
-            
+
             proguardFiles(
+                
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
