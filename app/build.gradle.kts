68d01ce2cad24b818873fb9ec9987667a79436dd
@hexfa <amir.dehdarian2009@gmail.com>
2025-07-28 23:13:33 -0800

    feat:update gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,8 +64,9 @@ dependencies {
     implementation(libs.androidx.material3)
 
     implementation(libs.androidx.compose.material)
-    
+
     implementation(libs.androidx.navigation.runtime.ktx)
+    
     implementation(libs.androidx.navigation.compose)
     implementation(libs.material)
     implementation(libs.androidx.media3.effect)
