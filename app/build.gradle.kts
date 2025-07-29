0fee8ea30a8e3d0d91ef6ee53f581461f82cd6a4
@hexfa <amir.dehdarian2009@gmail.com>
2025-07-28 23:13:30 -0800

    feat:update gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,8 +62,9 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
 
     implementation(libs.androidx.material3)
-    
+
     implementation(libs.androidx.compose.material)
+    
     implementation(libs.androidx.navigation.runtime.ktx)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.material)
