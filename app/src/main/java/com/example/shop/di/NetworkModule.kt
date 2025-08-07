365077e85b3dc89879fce1fa679f0f7d4d7478f2
@hexfa <amir.dehdarian2009@gmail.com>
2025-08-06 23:05:22 -0800

    feat:update NetworkModule

--- app/src/main/java/com/example/shop/di/NetworkModule.kt
+++ app/src/main/java/com/example/shop/di/NetworkModule.kt
@@ -36,7 +36,6 @@ object NetworkModule {
         .connectTimeout(TIMEOUT_IN_SECOND, TimeUnit.SECONDS)
 
         .readTimeout(TIMEOUT_IN_SECOND, TimeUnit.SECONDS)
-
         .writeTimeout(TIMEOUT_IN_SECOND, TimeUnit.SECONDS)
         .addInterceptor { chain ->
             val request = chain.request().newBuilder()
