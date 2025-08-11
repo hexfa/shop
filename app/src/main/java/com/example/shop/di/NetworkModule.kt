07d8e6a406eec37281db06ffcc517b4b46a6a8cf
@hexfa <amir.dehdarian2009@gmail.com>
2025-08-10 23:07:42 -0800

    feat:update NetworkModule

--- app/src/main/java/com/example/shop/di/NetworkModule.kt
+++ app/src/main/java/com/example/shop/di/NetworkModule.kt
@@ -49,6 +49,7 @@ object NetworkModule {
             .addConverterFactory(GsonConverterFactory.create())
             .client(okHttpClient)
             .build()
+
     @Provides
     @Singleton
     fun provideApiService(retrofit: Retrofit): HomeApiInterface =
