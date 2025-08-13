4e1df1291aa91d9bb99ab87f1bb6d202f915e177
@hexfa <amir.dehdarian2009@gmail.com>
2025-08-12 19:13:01 -0800

    feat:update NetworkModule

--- app/src/main/java/com/example/shop/di/NetworkModule.kt
+++ app/src/main/java/com/example/shop/di/NetworkModule.kt
@@ -45,8 +45,10 @@ object NetworkModule {
                 .addHeader("x-api-key", API_KEY)
 
                 .addHeader("lang", USER_LANGUAGE)
-            
+
             chain.proceed(request.build())
+
+            
         }
         .addInterceptor(interceptor())
         .build()
