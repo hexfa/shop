c0eb1f5ab70e8fa4b70ba368f0c785ccb6fa1b0b
@hexfa <amir.dehdarian2009@gmail.com>
2025-08-17 22:28:53 -0800

    feat:update NetworkModule

--- app/src/main/java/com/example/shop/di/NetworkModule.kt
+++ app/src/main/java/com/example/shop/di/NetworkModule.kt
@@ -45,6 +45,7 @@ object NetworkModule {
         .addInterceptor(interceptor())
         .build()
     @Provides
+    
     @Singleton
 
     fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit =
