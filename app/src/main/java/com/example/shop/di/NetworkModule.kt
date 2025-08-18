bc70de047cbc4a246ea05e16630c71516cd793a8
@hexfa <amir.dehdarian2009@gmail.com>
2025-08-17 22:28:47 -0800

    feat:update NetworkModule

--- app/src/main/java/com/example/shop/di/NetworkModule.kt
+++ app/src/main/java/com/example/shop/di/NetworkModule.kt
@@ -46,6 +46,7 @@ object NetworkModule {
         .build()
     @Provides
     @Singleton
+
     fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit =
         Retrofit.Builder()
             .baseUrl(BASE_URL)
