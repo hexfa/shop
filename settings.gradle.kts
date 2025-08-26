03d69b07cc35e6b7597256f4d137b7b0959b80a6
@hexfa <amir.dehdarian2009@gmail.com>
2025-08-25 22:52:47 -0800

    feat:provideApiService NetworkModule

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,8 +2,9 @@ pluginManagement {
 
     repositories {
         google {
-            
+
             content {
+                
                 includeGroupByRegex("com\\.android.*")
                 includeGroupByRegex("com\\.google.*")
                 includeGroupByRegex("androidx.*")
