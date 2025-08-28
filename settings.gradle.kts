7689e9370fd1044039eea382d1e972236da4e280
@hexfa <amir.dehdarian2009@gmail.com>
2025-08-27 16:44:20 -0800

    feat:provideApiService NetworkModule

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -26,8 +26,9 @@ dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
 
     repositories {
-        
+
         google()
+        
         mavenCentral()
     }
 }
