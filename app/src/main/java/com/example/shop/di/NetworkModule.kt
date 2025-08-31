c0ef32b02d334bd272605856a2669dbb96395554
@hexfa <amir.dehdarian2009@gmail.com>
2025-08-30 23:05:00 -0800

    feat: addInterceptor

--- app/src/main/java/com/example/shop/di/NetworkModule.kt
+++ app/src/main/java/com/example/shop/di/NetworkModule.kt
@@ -39,7 +39,6 @@ object NetworkModule {
         .addInterceptor { chain ->
             val request = chain.request().newBuilder()
 
-
                 .addHeader("x-api-key", API_KEY)
 
                 .addHeader("lang", USER_LANGUAGE)
