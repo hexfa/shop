c2abfd0c1895828250ae0fb34e80d51fbf3372a4
@hexfa <amir.dehdarian2009@gmail.com>
2025-08-30 23:05:27 -0800

    feat:addHeader  in Interceptor

--- app/src/main/java/com/example/shop/di/NetworkModule.kt
+++ app/src/main/java/com/example/shop/di/NetworkModule.kt
@@ -40,7 +40,6 @@ object NetworkModule {
             val request = chain.request().newBuilder()
                 .addHeader("x-api-key", API_KEY)
                 .addHeader("lang", USER_LANGUAGE)
-
             chain.proceed(request.build())
         }
 
