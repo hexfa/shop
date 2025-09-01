8ed9fe2cd1e45572f9c5dbcc08d61f8e489e13b1
@hexfa <amir.dehdarian2009@gmail.com>
2025-08-31 22:46:18 -0800

    feat:update BottomNavigationBar

--- app/src/main/java/com/example/shop/navigation/BottomNavigationBar.kt
+++ app/src/main/java/com/example/shop/navigation/BottomNavigationBar.kt
@@ -49,8 +49,9 @@ fun BottomNavigationBar(
         BottomNavItem(
 
             name = stringResource(id = R.string.home),
-            
+
             route = Screen.Home.route,
+            
             selectedIcon = painterResource(R.drawable.home_fill),
             deSelectedIcon = painterResource(R.drawable.home_outline)
         ),
