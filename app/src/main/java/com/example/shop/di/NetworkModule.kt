76f731c436207a12cf536c179cd492c3d8fe8648
@hexfa <amir.dehdarian2009@gmail.com>
2025-09-02 18:01:14 -0800

    feat:fixed provideRetrofit

--- app/src/main/java/com/example/shop/di/NetworkModule.kt
+++ app/src/main/java/com/example/shop/di/NetworkModule.kt
@@ -52,7 +52,6 @@ object NetworkModule {
             .baseUrl(BASE_URL
             .addConverterFactory(GsonConverterFactory.create())
             .client(okHttpClient)
-
             .build()
     @Provides
     @Singleton
