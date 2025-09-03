dcc186e500207e7f189a07a8f29ed4e67f82783c
@hexfa <amir.dehdarian2009@gmail.com>
2025-09-02 18:00:49 -0800

    feat:update addHeader

--- app/src/main/java/com/example/shop/di/NetworkModule.kt
+++ app/src/main/java/com/example/shop/di/NetworkModule.kt
@@ -49,7 +49,6 @@ object NetworkModule {
     @Singleton
     fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit =
         Retrofit.Builder()
-
             .baseUrl(BASE_URL)
 
             .addConverterFactory(GsonConverterFactory.create())
