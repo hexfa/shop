c0dfb96afba4d720dc1985682b6f2bca1386139a
@hexfa <amir.dehdarian2009@gmail.com>
2025-09-02 18:01:37 -0800

    feat:fixed provideRetrofit

--- app/src/main/java/com/example/shop/di/NetworkModule.kt
+++ app/src/main/java/com/example/shop/di/NetworkModule.kt
@@ -32,7 +32,6 @@ object NetworkModule {
 
 
     fun provideOkHttp(): OkHttpClient = OkHttpClient.Builder()
-
         .connectTimeout(TIMEOUT_IN_SECOND, TimeUnit.SECONDS)
         .readTimeout(TIMEOUT_IN_SECOND, TimeUnit.SECONDS)
         .writeTimeout(TIMEOUT_IN_SECOND, TimeUnit.SECONDS)
