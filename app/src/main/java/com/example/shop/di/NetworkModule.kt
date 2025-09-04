cdfcd97d511029bd66bec55d8947fde41d1b522a
@hexfa <amir.dehdarian2009@gmail.com>
2025-09-03 22:58:31 -0800

    feat:fixed provideRetrofit

--- app/src/main/java/com/example/shop/di/NetworkModule.kt
+++ app/src/main/java/com/example/shop/di/NetworkModule.kt
@@ -50,8 +50,9 @@ object NetworkModule {
     @Provides
     @Singleton
     fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit =
-        
+
         Retrofit.Builder()
+            
             .baseUrl(BASE_URL
             .addConverterFactory(GsonConverterFactory.create())
             .client(okHttpClient)
