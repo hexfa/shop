2090c3d52740a6c8de37d37653ea1a9ff3723ff6
@hexfa <amir.dehdarian2009@gmail.com>
2025-09-03 22:58:06 -0800

    feat:fixed provideRetrofit

--- app/src/main/java/com/example/shop/di/NetworkModule.kt
+++ app/src/main/java/com/example/shop/di/NetworkModule.kt
@@ -39,6 +39,7 @@ object NetworkModule {
                 .addHeader("x-api-key", API_KEY)
 
                 .addHeader("lang", USER_LANGUAGE)
+            
             chain.proceed(request.build())
         }
         .addInterceptor(interceptor())
