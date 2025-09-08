21c24ffe4eacbd86e9061b03f0109c5553441abb
@hexfa <amir.dehdarian2009@gmail.com>
2025-09-08 11:34:53 -0800

    feat:fixed provideOkHttp

--- app/src/main/java/com/example/shop/di/NetworkModule.kt
+++ app/src/main/java/com/example/shop/di/NetworkModule.kt
@@ -46,7 +46,6 @@ object NetworkModule {
                 .addHeader("x-api-key", API_KEY)
 
                 .addHeader("lang", USER_LANGUAGE)
-
             chain.proceed(request.build())
         }
         .addInterceptor(interceptor())
