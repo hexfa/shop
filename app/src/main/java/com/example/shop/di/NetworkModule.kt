ef81ebcabaa848a3cb1474c746dd5bf85250d1a5
@hexfa <amir.dehdarian2009@gmail.com>
2025-09-08 11:34:11 -0800

    feat:fixed provideOkHttp

--- app/src/main/java/com/example/shop/di/NetworkModule.kt
+++ app/src/main/java/com/example/shop/di/NetworkModule.kt
@@ -63,7 +63,6 @@ object NetworkModule {
 
 
         Retrofit.Builder()
-
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .client(okHttpClient)
