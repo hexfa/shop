d279f350b2cc0302a737c07675ac0bd91e225799
@hexfa <amir.dehdarian2009@gmail.com>
2025-09-08 11:32:32 -0800

    feat:fixed provideOkHttp

--- app/src/main/java/com/example/shop/di/NetworkModule.kt
+++ app/src/main/java/com/example/shop/di/NetworkModule.kt
@@ -28,6 +28,7 @@ object NetworkModule {
         return logging
     }
     @Provides
+
     @Singleton
     fun provideOkHttp(): OkHttpClient = OkHttpClient.Builder()
         .connectTimeout(TIMEOUT_IN_SECOND, TimeUnit.SECONDS)
