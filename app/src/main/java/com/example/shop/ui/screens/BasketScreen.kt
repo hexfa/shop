c7d025d6f6cb7e49936b93c2ba067c033ec1bb25
@hexfa <amir.dehdarian2009@gmail.com>
2025-09-07 23:33:26 -0800

    feat:fixed BasketScreen

--- app/src/main/java/com/example/shop/ui/screens/BasketScreen.kt
+++ app/src/main/java/com/example/shop/ui/screens/BasketScreen.kt
@@ -15,7 +15,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import com.example.shop.ui.theme.selectedBottomBar
-
 @Composable
 fun BasketScreen(navController: NavHostController) {
     if (isSystemInDarkTheme()) {
