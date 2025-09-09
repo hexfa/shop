f7c4f984c1db973e5437c7ff994fd0d33b5f4433
@hexfa <amir.dehdarian2009@gmail.com>
2025-09-09 11:37:33 -0800

    feat:fixed Screens

--- app/src/main/java/com/example/shop/navigation/Screen.kt
+++ app/src/main/java/com/example/shop/navigation/Screen.kt
@@ -5,7 +5,6 @@ sealed class Screen(val route: String) {
     object Splash : Screen("splash_screen")
 
     object Home : Screen("home_screen")
-
     object Basket : Screen("basket_screen")
     object Profile : Screen("profile_screen")
     object Category : Screen("category_screen")
