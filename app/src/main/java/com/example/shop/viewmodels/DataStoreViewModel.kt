fe7fc87208f7e1be248618967efbe0ce07215fd8
@hexfa <amir.dehdarian2009@gmail.com>
2025-09-10 13:47:03 -0800

    feat:update DataStoreViewModel

--- app/src/main/java/com/example/shop/viewmodels/DataStoreViewModel.kt
+++ app/src/main/java/com/example/shop/viewmodels/DataStoreViewModel.kt
@@ -17,8 +17,7 @@ class DataStoreViewModel @Inject constructor(
         const val USER_LANGUAGE_KEY = "USER_LANGUAGE_KEY"
     }
 
-
-
+    
     fun saveUserLanguage(value: String) {
         viewModelScope.launch {
             repository.putString(USER_LANGUAGE_KEY, value)
