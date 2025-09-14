6e5fa1c4f3af1db2d2ff8eb0596fba69c0b9f682
@hexfa <amir.dehdarian2009@gmail.com>
2025-09-13 23:29:09 -0800

    feat:update LaunchedEffect

--- app/src/main/java/com/example/shop/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/shop/ui/screens/home/HomeScreen.kt
@@ -44,9 +44,10 @@ fun Home(
             .background(color = Color.White)
 
             .fillMaxSize()
-        
+
     ) {
         val refreshScope = rememberCoroutineScope()
+        
         val swipeRefreshState = rememberSwipeRefreshState(isRefreshing = false)
         SwipeRefresh(state = swipeRefreshState, onRefresh = {
             refreshScope.launch {
