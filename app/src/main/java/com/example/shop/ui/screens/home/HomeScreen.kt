95b59ee052ac36f9d37da7a04da30eab6d813768
@hexfa <amir.dehdarian2009@gmail.com>
2025-09-14 23:33:04 -0800

    feat:update LaunchedEffect

--- app/src/main/java/com/example/shop/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/shop/ui/screens/home/HomeScreen.kt
@@ -51,7 +51,6 @@ fun Home(
                 Modifier
                     .background(color = Color.White)
                     .fillMaxSize()
-
                     .verticalScroll(rememberScrollState())
 
                     .padding(bottom = 60.dp)
