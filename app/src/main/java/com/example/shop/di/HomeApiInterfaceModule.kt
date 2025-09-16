57ab4f3942b81ef05b733441d68c2300db33eea8
@hexfa <amir.dehdarian2009@gmail.com>
2025-09-15 16:14:41 -0800

    feat:update HomeApiInterfaceModule

--- app/src/main/java/com/example/shop/di/HomeApiInterfaceModule.kt
+++ app/src/main/java/com/example/shop/di/HomeApiInterfaceModule.kt
@@ -17,6 +17,7 @@ object HomeApiInterfaceModule {
     @Singleton
 
     fun provideHomeApiService(retrofit: Retrofit): HomeApiInterface =
-        
+
         retrofit.create(HomeApiInterface::class.java)
+
 }
