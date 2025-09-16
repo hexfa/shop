de9b8c14597c9f43bfbcdbe28b345dc9561d0372
@hexfa <amir.dehdarian2009@gmail.com>
2025-09-15 16:15:26 -0800

    feat:change navBackStackEntry

--- app/src/main/java/com/example/shop/ui/components/ChangeStatusBarColor.kt
+++ app/src/main/java/com/example/shop/ui/components/ChangeStatusBarColor.kt
@@ -15,6 +15,7 @@ fun ChangeStatusBarColor(navController: NavHostController) {
     val navBackStackEntry by navController.currentBackStackEntryAsState()
 
     val systemUiController = rememberSystemUiController()
+
     when (navBackStackEntry?.destination?.route) {
         Screen.Splash.route -> {
             SideEffect {
