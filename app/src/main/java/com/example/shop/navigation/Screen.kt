1069ef527831d8b0877e850578fa8f0fed8b8947
@hexfa <amir.dehdarian2009@gmail.com>
2025-09-17 18:48:50 -0800

    feat:change HomeScreen

--- app/src/main/java/com/example/shop/navigation/Screen.kt
+++ app/src/main/java/com/example/shop/navigation/Screen.kt
@@ -1,7 +1,8 @@
 package com.example.shop.navigation
 
 sealed class Screen(val route: String) {
-    object Splash : Screen("splash_screen")
+
+   object Splash : Screen("splash_screen")
 
     object Home : Screen("home_screen")
     object Basket : Screen("basket_screen")
