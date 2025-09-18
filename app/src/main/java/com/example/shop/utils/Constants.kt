a87f2b4b76cf03cf6f5a34db7433abfe88541c8e
@hexfa <amir.dehdarian2009@gmail.com>
2025-09-17 18:52:15 -0800

    feat:change HomeScreen

--- app/src/main/java/com/example/shop/utils/Constants.kt
+++ app/src/main/java/com/example/shop/utils/Constants.kt
@@ -5,7 +5,8 @@ import com.google.gson.internal.GsonBuildConfig
 object Constants {
 
     const val ENGLISH="en"
-    const val PERSIAN="fa"
+
+   const val PERSIAN="fa"
     const val DATASTORE_NAME="SHOP_DATA_STORE"
     const val TIMEOUT_IN_SECOND:Long=60
     const val BASE_URL="https://truelearn-digikala.iran.liara.run/api/"
