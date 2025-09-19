ac974853e2c8d41f1b09d2277fd6def94508812e
@hexfa <amir.dehdarian2009@gmail.com>
2025-09-18 20:29:31 -0800

    feat:add Constants

--- app/src/main/java/com/example/shop/utils/Constants.kt
+++ app/src/main/java/com/example/shop/utils/Constants.kt
@@ -8,7 +8,6 @@ object Constants {
    const val PERSIAN="fa"
     const val DATASTORE_NAME="SHOP_DATA_STORE"
     const val TIMEOUT_IN_SECOND:Long=60
-
     const val BASE_URL="https://truelearn-digikala.iran.liara.run/api/"
 
     const val API_KEY=GsonBuildConfig.VERSION
