4b28dfdd3f815e9444dafa795cd5259e6acf2e5c
@hexfa <amir.dehdarian2009@gmail.com>
2025-09-20 06:55:32 -0800

    feat:add HiltViewModel

--- app/src/main/java/com/example/shop/viewmodels/HomeViewModel.kt
+++ app/src/main/java/com/example/shop/viewmodels/HomeViewModel.kt
@@ -14,7 +14,8 @@ import javax.inject.Inject
 class HomeViewModel @Inject constructor(private val homeRepo: HomeRepository):ViewModel(){
 
     val slider= MutableStateFlow<NetworkResult<List<Slider>>>(NetworkResult.Loading())
-    
+
+
 
      fun getSlider(){
          viewModelScope.launch {
