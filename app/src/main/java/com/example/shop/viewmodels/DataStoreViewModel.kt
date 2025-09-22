5847bd464239a71b33169702c32f1c6a4ee47a46
@hexfa <amir.dehdarian2009@gmail.com>
2025-09-21 22:45:06 -0800

    feat:SaveUserLanguage

--- app/src/main/java/com/example/shop/viewmodels/DataStoreViewModel.kt
+++ app/src/main/java/com/example/shop/viewmodels/DataStoreViewModel.kt
@@ -22,8 +22,9 @@ class DataStoreViewModel @Inject constructor(
         viewModelScope.launch {
 
             repository.putString(USER_LANGUAGE_KEY, value)
-            
+
         }
+        
     }
     fun getUserLanguage(): String = runBlocking {
         repository.getString(USER_LANGUAGE_KEY) ?: Constants.PERSIAN
