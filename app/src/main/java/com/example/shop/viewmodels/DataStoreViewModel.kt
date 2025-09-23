916e93389df542a46cbce97d270f9a47534cbacf
@hexfa <amir.dehdarian2009@gmail.com>
2025-09-22 18:20:43 -0800

    feat:DataStoreViewModel

--- app/src/main/java/com/example/shop/viewmodels/DataStoreViewModel.kt
+++ app/src/main/java/com/example/shop/viewmodels/DataStoreViewModel.kt
@@ -23,7 +23,6 @@ class DataStoreViewModel @Inject constructor(
         }
     }
     fun getUserLanguage(): String = runBlocking {
-
         repository.getString(USER_LANGUAGE_KEY) ?: Constants.PERSIAN
 
 
