cc829bbed84802cb4a8189c0e4fcddaa960b4dab
@hexfa <amir.dehdarian2009@gmail.com>
2025-09-23 09:42:12 -0800

    feat:fixed slider in HomeScreen

--- app/src/main/java/com/example/shop/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/shop/ui/screens/home/HomeScreen.kt
@@ -55,7 +55,6 @@ fun Home(
                 Modifier
 
                     .background(color = Color.White)
-                    
                     .fillMaxSize()
                     .verticalScroll(rememberScrollState())
                     .padding(bottom = 60.dp)
