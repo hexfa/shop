8a9866c3b1f532081ab98f98f55448bdca2784d2
@hexfa <amir.dehdarian2009@gmail.com>
2025-09-23 09:40:55 -0800

    feat:refreshScope in HomeScreen

--- app/src/main/java/com/example/shop/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/shop/ui/screens/home/HomeScreen.kt
@@ -44,8 +44,7 @@ fun Home(
         val refreshScope = rememberCoroutineScope()
         val swipeRefreshState = rememberSwipeRefreshState(isRefreshing = false)
         SwipeRefresh(state = swipeRefreshState, onRefresh = {
-
-
+            
             refreshScope.launch {
             }
         }) {
