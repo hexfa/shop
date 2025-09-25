a57cd3dee87fc96d12fd46dec79f1ed05d2af4a3
@hexfa <amir.dehdarian2009@gmail.com>
2025-09-24 23:04:10 -0800

    feat:fixed refreshScope in HomeScreen

--- app/src/main/java/com/example/shop/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/shop/ui/screens/home/HomeScreen.kt
@@ -50,8 +50,9 @@ fun Home(
         SwipeRefresh(state = swipeRefreshState, onRefresh = {
 
             refreshScope.launch {
-                
+
             }
+            
         }) {
             Column(
                 modifier =
