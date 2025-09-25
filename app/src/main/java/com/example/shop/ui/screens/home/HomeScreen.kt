e0209c6be637a68e66f7969f2da3430891a7ab58
@hexfa <amir.dehdarian2009@gmail.com>
2025-09-24 23:04:46 -0800

    feat:fixed refreshScope in HomeScreen

--- app/src/main/java/com/example/shop/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/shop/ui/screens/home/HomeScreen.kt
@@ -62,8 +62,9 @@ fun Home(
                 Modifier
 
                     .background(color = Color.White)
-                    
+
                     .fillMaxSize()
+                    
                     .verticalScroll(rememberScrollState())
                     .padding(bottom = 60.dp)
             ) {
