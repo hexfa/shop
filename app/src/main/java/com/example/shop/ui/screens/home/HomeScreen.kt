8c45a885ff9450aafe64b66c1a11b7931a2c7ef7
@hexfa <amir.dehdarian2009@gmail.com>
2025-09-28 22:33:43 -0800

    feat:update NavHostController in HomeScreen

--- app/src/main/java/com/example/shop/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/shop/ui/screens/home/HomeScreen.kt
@@ -59,7 +59,6 @@ fun Home(
                 Modifier
                     .background(color = Color.White)
                     .fillMaxSize()
-
                     .verticalScroll(rememberScrollState())
 
                     .padding(bottom = 60.dp)
