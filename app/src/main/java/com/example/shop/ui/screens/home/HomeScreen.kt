86b2c3461a0116bc019ff7db6e74ca03f8eed513
@hexfa <amir.dehdarian2009@gmail.com>
2025-09-28 22:34:19 -0800

    feat:update NavHostController in HomeScreen

--- app/src/main/java/com/example/shop/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/shop/ui/screens/home/HomeScreen.kt
@@ -60,11 +60,9 @@ fun Home(
                     .background(color = Color.White)
                     .fillMaxSize()
                     .verticalScroll(rememberScrollState())
-
                     .padding(bottom = 60.dp)
             ) {
                 LaunchedEffect(true) {
-
                     viewModel.getSlider();
 
 
