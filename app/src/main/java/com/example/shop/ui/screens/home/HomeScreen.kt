ded117432b65141592c0d5b17e34aa2122f1d827
@hexfa <amir.dehdarian2009@gmail.com>
2025-09-29 22:05:39 -0800

    feat:update NavHostController in HomeScreen

--- app/src/main/java/com/example/shop/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/shop/ui/screens/home/HomeScreen.kt
@@ -64,8 +64,9 @@ fun Home(
         })
 
         {
-            
+
             Column(
+                
                 modifier =
                 Modifier
                     .background(color = Color.White)
