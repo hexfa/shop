a10d10ff98ef8c72aff574778484876c4b801a1e
@hexfa <amir.dehdarian2009@gmail.com>
2025-09-29 22:05:10 -0800

    feat:update NavHostController in HomeScreen

--- app/src/main/java/com/example/shop/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/shop/ui/screens/home/HomeScreen.kt
@@ -50,9 +50,10 @@ fun Home(
 
 
         val swipeRefreshState = rememberSwipeRefreshState(isRefreshing = false)
-        
+
 
         SwipeRefresh(state = swipeRefreshState, onRefresh = {
+            
 
             refreshScope.launch {
 
