8c8d27c1c0d5d9f1e9a96657e5ccd0c1176a0a14
@hexfa <amir.dehdarian2009@gmail.com>
2025-10-01 23:38:07 -0800

    feat:update NavHostController

--- app/src/main/java/com/example/shop/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/shop/ui/screens/home/HomeScreen.kt
@@ -24,6 +24,7 @@ fun HomeScreen(navController: NavHostController) {
     Home(navController)
 }
 @Composable
+
 fun Home(
     navController: NavHostController,
     viewModel: HomeViewModel = hiltViewModel()
