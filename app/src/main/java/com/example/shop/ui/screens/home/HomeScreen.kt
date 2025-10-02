145c719ee178ea18543ae6e197d5eb22690f3edd
@hexfa <amir.dehdarian2009@gmail.com>
2025-10-02 09:55:14 -0800

    feat:change swipeRefreshState in HomeScreen

--- app/src/main/java/com/example/shop/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/shop/ui/screens/home/HomeScreen.kt
@@ -51,6 +51,7 @@ fun Home(
                     .verticalScroll(rememberScrollState())
                     .padding(bottom = 60.dp)
             ) {
+                
                 LaunchedEffect(true) {
                     viewModel.getSlider();
                 }
