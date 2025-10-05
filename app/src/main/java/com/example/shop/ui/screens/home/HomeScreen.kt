0bb2fdfce01eb5982845ea516d3011057b54e67b
@hexfa <amir.dehdarian2009@gmail.com>
2025-10-04 17:38:56 -0800

    feat:change refreshScope in HomeScreen

--- app/src/main/java/com/example/shop/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/shop/ui/screens/home/HomeScreen.kt
@@ -44,8 +44,9 @@ fun Home(
         })
         {
 
-            
+
             Column(
+                
                 modifier =
                 Modifier
                     .background(color = Color.White)
