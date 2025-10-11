b66519e997ba60e491d526b899ff94656d23cf1e
@hexfa <amir.dehdarian2009@gmail.com>
2025-10-11 13:37:59 -0800

    feat:change verticalScroll in HomeScreen

--- app/src/main/java/com/example/shop/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/shop/ui/screens/home/HomeScreen.kt
@@ -62,7 +62,6 @@ fun Home(
                     .fillMaxSize()
 
                     .verticalScroll(rememberScrollState())
-
                     .padding(bottom = 60.dp)
             ) {
                 LaunchedEffect(true) {
