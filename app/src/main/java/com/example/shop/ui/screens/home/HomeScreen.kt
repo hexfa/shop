cda3ef878b80ae5f5314d6f2ee21f3c31023114d
@hexfa <amir.dehdarian2009@gmail.com>
2025-10-11 13:34:59 -0800

    feat:change NavHostController in HomeScreen

--- app/src/main/java/com/example/shop/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/shop/ui/screens/home/HomeScreen.kt
@@ -51,9 +51,10 @@ fun Home(
                     .verticalScroll(rememberScrollState())
 
                     .padding(bottom = 60.dp)
-                
+
             ) {
                 LaunchedEffect(true) {
+                    
                     viewModel.getSlider();
                 }
                 TopSlider()
