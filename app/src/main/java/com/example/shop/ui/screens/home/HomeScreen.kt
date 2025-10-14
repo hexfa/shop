8b25d7e25a92e2b9a142fc3af78c6d6e31aed70e
@hexfa <amir.dehdarian2009@gmail.com>
2025-10-13 22:46:45 -0800

    feat:update TopSlider in HomeScreen

--- app/src/main/java/com/example/shop/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/shop/ui/screens/home/HomeScreen.kt
@@ -54,7 +54,6 @@ fun Home(
                     .background(color = Color.White)
                     .fillMaxSize()
                     .verticalScroll(rememberScrollState())
-
                     .padding(bottom = 60.dp)
 
             ) {
