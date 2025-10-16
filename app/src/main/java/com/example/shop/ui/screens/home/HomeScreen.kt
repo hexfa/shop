78b0d049531a457384b5e8db7b44da1516e85351
@hexfa <amir.dehdarian2009@gmail.com>
2025-10-15 22:57:43 -0800

    feat:fixed bug in swipeRefreshState

--- app/src/main/java/com/example/shop/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/shop/ui/screens/home/HomeScreen.kt
@@ -39,7 +39,6 @@ fun Home(
 
         val swipeRefreshState = rememberSwipeRefreshState(isRefreshing = false)
         SwipeRefresh(state = swipeRefreshState, onRefresh = {
-
             refreshScope.launch {
 
             }
