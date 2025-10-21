9071332b55265106bc1c87de720969a5a5d3248c
@hexfa <amir.dehdarian2009@gmail.com>
2025-10-20 22:33:39 -0800

    feat:update HomeScreen

--- app/src/main/java/com/example/shop/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/shop/ui/screens/home/HomeScreen.kt
@@ -39,8 +39,9 @@ fun Home(
         val swipeRefreshState = rememberSwipeRefreshState(isRefreshing = false)
 
         SwipeRefresh(state = swipeRefreshState, onRefresh = {
-            
+
             refreshScope.launch {
+                
             }
         })
         {
