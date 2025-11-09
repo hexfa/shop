a92923e10f32c7233eabb1183af6af95d562aeb6
@hexfa <amir.dehdarian2009@gmail.com>
2025-11-08 23:32:56 -0800

    feat:update HomeScreen

--- app/src/main/java/com/example/shop/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/shop/ui/screens/home/HomeScreen.kt
@@ -55,7 +55,6 @@ fun Home(
                     .background(color = Color.White)
                     .fillMaxSize()
                     .verticalScroll(rememberScrollState())
-
                     .padding(bottom = 60.dp)
 
             ) {
