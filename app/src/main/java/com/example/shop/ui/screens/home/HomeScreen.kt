2e660dc49414ca2a062945bdcc60b876dc80fe30
@hexfa <amir.dehdarian2009@gmail.com>
2025-11-09 23:23:34 -0800

    feat:update refreshScope

--- app/src/main/java/com/example/shop/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/shop/ui/screens/home/HomeScreen.kt
@@ -59,8 +59,9 @@ fun Home(
                 LaunchedEffect(true) {
 
                     viewModel.getSlider();
-                    
+
                 }
+                
                 TopSlider()
             }
         }
