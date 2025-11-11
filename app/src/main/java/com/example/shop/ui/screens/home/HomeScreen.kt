26d3b0496f86022847779c6d34429cbf6fc6c6d9
@hexfa <amir.dehdarian2009@gmail.com>
2025-11-10 22:41:13 -0800

    feat:update refreshScope

--- app/src/main/java/com/example/shop/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/shop/ui/screens/home/HomeScreen.kt
@@ -47,7 +47,6 @@ fun Home(
                     .fillMaxSize()
                     .verticalScroll(rememberScrollState())
                     .padding(bottom = 60.dp)
-
             ) {
 
                 LaunchedEffect(true) {
