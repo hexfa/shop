ae46cb435a2a4535355c2891818e70420337a3a1
@hexfa <amir.dehdarian2009@gmail.com>
2025-11-22 22:30:45 -0800

    feat:update refreshScope

--- app/src/main/java/com/example/shop/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/shop/ui/screens/home/HomeScreen.kt
@@ -51,8 +51,9 @@ fun Home(
                     .background(color = Color.White)
 
                     .fillMaxSize()
-                    
+
                     .verticalScroll(rememberScrollState())
+                    
                     .padding(bottom = 60.dp)
             ) {
 
