15981ae7f45a4a23d8c3a7880e08930cf34e09f8
@hexfa <amir.dehdarian2009@gmail.com>
2025-11-29 22:59:09 -0800

    feat:update refreshScope

--- app/src/main/java/com/example/shop/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/shop/ui/screens/home/HomeScreen.kt
@@ -52,7 +52,6 @@ fun Home(
                     .verticalScroll(rememberScrollState())
                     .padding(bottom = 60.dp)
             ) {
-
                 LaunchedEffect(true) {
 
                     viewModel.getSlider();
