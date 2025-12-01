2c8ad3eec4a3077c6246c6b30475fdc410dd750d
@hexfa <amir.dehdarian2009@gmail.com>
2025-11-30 23:32:17 -0800

    feat:update DataStoreRepository

--- app/src/main/java/com/example/shop/data/datastore/DataStoreRepository.kt
+++ app/src/main/java/com/example/shop/data/datastore/DataStoreRepository.kt
@@ -3,7 +3,6 @@ package com.example.shop.data.datastore
 interface DataStoreRepository {
     suspend fun putString(key:String,value:String)
     suspend fun putInt(key:String,value:Int)
-
     suspend fun getString(key:String):String?
 
     suspend fun getInt(key:String):Int?
