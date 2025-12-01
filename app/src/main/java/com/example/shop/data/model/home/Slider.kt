7aae5c5cdde8d5eceff665a02f4b4914ad9b4cbd
@hexfa <amir.dehdarian2009@gmail.com>
2025-11-30 23:33:45 -0800

    feat:update Slider

--- app/src/main/java/com/example/shop/data/model/home/Slider.kt
+++ app/src/main/java/com/example/shop/data/model/home/Slider.kt
@@ -1,10 +1,11 @@
 package com.example.shop.data.model.home
 
 data class Slider(
+
     val _id:String,
     val image:String,
     val url:String,
     val category:String,
     val priority:Int
-    
+
 )
