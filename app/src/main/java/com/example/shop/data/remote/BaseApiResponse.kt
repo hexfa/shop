160da84aa016105ff877ac014aa2c85a3f409108
@hexfa <amir.dehdarian2009@gmail.com>
2025-12-02 11:14:04 -0800

    feat:update safeApiCall

--- app/src/main/java/com/example/shop/data/remote/BaseApiResponse.kt
+++ app/src/main/java/com/example/shop/data/remote/BaseApiResponse.kt
@@ -27,8 +27,9 @@ open class BaseApiResponse {
                     }
 
                 }
-                
+
                 return@withContext error("code :${response.code()}  message:${response.message()}")
+
             } catch (e: Exception) {
                 return@withContext error(e.message ?: e.toString())
             }
