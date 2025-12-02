abb9abc8bc14b540934db8c6b0f532d3f2accb61
@hexfa <amir.dehdarian2009@gmail.com>
2025-12-02 11:14:33 -0800

    feat:update safeApiCall

--- app/src/main/java/com/example/shop/data/remote/BaseApiResponse.kt
+++ app/src/main/java/com/example/shop/data/remote/BaseApiResponse.kt
@@ -17,7 +17,6 @@ open class BaseApiResponse {
                 if (response.isSuccessful) {
 
                     val body = response.body()
-
                     body?.let {
 
                         return@withContext NetworkResult.Success(body.message, body.data)
