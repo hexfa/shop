081edf4d6e90a9ca1d4e49fedf23094e13a5c9ca
@hexfa <amir.dehdarian2009@gmail.com>
2025-12-02 11:14:28 -0800

    feat:update safeApiCall

--- app/src/main/java/com/example/shop/data/remote/BaseApiResponse.kt
+++ app/src/main/java/com/example/shop/data/remote/BaseApiResponse.kt
@@ -20,7 +20,6 @@ open class BaseApiResponse {
 
                     body?.let {
 
-
                         return@withContext NetworkResult.Success(body.message, body.data)
                     }
                 }
