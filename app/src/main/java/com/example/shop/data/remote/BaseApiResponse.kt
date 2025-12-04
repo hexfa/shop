ec3fb76ef4d72d2b048d6882befb45d55f141a49
@hexfa <amir.dehdarian2009@gmail.com>
2025-12-03 23:06:02 -0800

    feat:update safeApiCall

--- app/src/main/java/com/example/shop/data/remote/BaseApiResponse.kt
+++ app/src/main/java/com/example/shop/data/remote/BaseApiResponse.kt
@@ -7,9 +7,10 @@ import retrofit2.Response
 
 open class BaseApiResponse {
     suspend fun <T> safeApiCall(apiCall: suspend () -> Response<ResponseResult<T>>): NetworkResult<T> =
+
         withContext(Dispatchers.IO) {
             try {
-                
+
                 val response = apiCall()
 
                 if (response.isSuccessful) {
