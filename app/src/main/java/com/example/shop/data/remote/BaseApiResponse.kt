611bcc271cad60c2aa12309e17c0fb5a4c298ac3
@hexfa <amir.dehdarian2009@gmail.com>
2025-12-08 23:23:50 -0800

    feat:update BaseApiResponse

--- app/src/main/java/com/example/shop/data/remote/BaseApiResponse.kt
+++ app/src/main/java/com/example/shop/data/remote/BaseApiResponse.kt
@@ -9,7 +9,6 @@ open class BaseApiResponse {
 
     suspend fun <T> safeApiCall(apiCall: suspend () -> Response<ResponseResult<T>>): NetworkResult<T> =
         withContext(Dispatchers.IO) {
-
             try {
 
                 val response = apiCall()
