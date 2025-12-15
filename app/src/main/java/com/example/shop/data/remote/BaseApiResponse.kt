9726ae9fc5c071ef1b945f5cb965909cad33eef8
@hexfa <amir.dehdarian2009@gmail.com>
2025-12-14 23:10:10 -0800

    feat:update BaseApiResponse

--- app/src/main/java/com/example/shop/data/remote/BaseApiResponse.kt
+++ app/src/main/java/com/example/shop/data/remote/BaseApiResponse.kt
@@ -21,9 +21,10 @@ open class BaseApiResponse {
                 if (response.isSuccessful) {
 
                     val body = response.body()
-                    
+
 
                     body?.let {
+                        
 
                         return@withContext NetworkResult.Success(body.message, body.data)
 
