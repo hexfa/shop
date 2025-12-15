0cffe6e3f866e0973a12246d24405810e647004c
@hexfa <amir.dehdarian2009@gmail.com>
2025-12-14 23:10:14 -0800

    feat:update BaseApiResponse

--- app/src/main/java/com/example/shop/data/remote/BaseApiResponse.kt
+++ app/src/main/java/com/example/shop/data/remote/BaseApiResponse.kt
@@ -24,13 +24,14 @@ open class BaseApiResponse {
 
 
                     body?.let {
-                        
+
 
                         return@withContext NetworkResult.Success(body.message, body.data)
 
                     }
 
                 }
+
 
                 return@withContext error("code :${response.code()}  message:${response.message()}")
             } catch (e: Exception) {
