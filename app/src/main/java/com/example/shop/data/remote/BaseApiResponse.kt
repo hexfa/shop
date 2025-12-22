45e973efb56f499339fb264d6d9353942c451f13
@hexfa <amir.dehdarian2009@gmail.com>
2025-12-22 10:22:54 -0800

    feat:fixed safeApiCall

--- app/src/main/java/com/example/shop/data/remote/BaseApiResponse.kt
+++ app/src/main/java/com/example/shop/data/remote/BaseApiResponse.kt
@@ -15,7 +15,6 @@ open class BaseApiResponse {
                     body?.let {
                         return@withContext NetworkResult.Success(body.message, body.data)
                     }
-
                 }
                 return@withContext error("code :${response.code()}  message:${response.message()}")
 
