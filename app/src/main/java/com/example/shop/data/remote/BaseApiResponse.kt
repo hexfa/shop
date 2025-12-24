d6508d66063451b1e6623d221c65e2cecc44e7a4
@hexfa <amir.dehdarian2009@gmail.com>
2025-12-23 23:19:36 -0800

    feat:fixed safeApiCall

--- app/src/main/java/com/example/shop/data/remote/BaseApiResponse.kt
+++ app/src/main/java/com/example/shop/data/remote/BaseApiResponse.kt
@@ -35,8 +35,9 @@ open class BaseApiResponse {
 
                 return@withContext error(e.message ?: e.toString())
             }
-            
+
         }
+
     private fun <T> error(errorMessage: String): NetworkResult<T> =
         NetworkResult.Error("Api call failed: $errorMessage")
 }
