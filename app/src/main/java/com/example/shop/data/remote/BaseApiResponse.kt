123ddafc5c3b6061876e799f94c76e7744d6f0c4
@hexfa <amir.dehdarian2009@gmail.com>
2025-12-24 11:03:53 -0800

    feat:fixed BaseApiResponse

--- app/src/main/java/com/example/shop/data/remote/BaseApiResponse.kt
+++ app/src/main/java/com/example/shop/data/remote/BaseApiResponse.kt
@@ -20,7 +20,6 @@ open class BaseApiResponse {
 
                 return@withContext error("code :${response.code()}  message:${response.message()}")
 
-
             } catch (e: Exception) {
 
                 return@withContext error(e.message ?: e.toString())
