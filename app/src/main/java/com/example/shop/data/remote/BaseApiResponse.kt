c52516837f627617d717d0502370925e24190f0e
@hexfa <amir.dehdarian2009@gmail.com>
2025-12-28 23:34:16 -0800

    feat:fixed BaseApiResponse

--- app/src/main/java/com/example/shop/data/remote/BaseApiResponse.kt
+++ app/src/main/java/com/example/shop/data/remote/BaseApiResponse.kt
@@ -32,8 +32,9 @@ open class BaseApiResponse {
             } catch (e: Exception) {
 
                 return@withContext error(e.message ?: e.toString())
-                
+
             }
+
 
         }
     private fun <T> error(errorMessage: String): NetworkResult<T> =
