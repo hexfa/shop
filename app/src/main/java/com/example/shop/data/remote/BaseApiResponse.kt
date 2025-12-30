273a23293500c16949a0f65c50c7216799daa51c
@hexfa <amir.dehdarian2009@gmail.com>
2025-12-29 22:56:40 -0800

    feat:fixed BaseApiResponse

--- app/src/main/java/com/example/shop/data/remote/BaseApiResponse.kt
+++ app/src/main/java/com/example/shop/data/remote/BaseApiResponse.kt
@@ -15,7 +15,6 @@ open class BaseApiResponse {
 
                 val response = apiCall()
                 if (response.isSuccessful) {
-
                     val body = response.body()
 
                     body?.let {
