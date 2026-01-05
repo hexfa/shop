287b53bae51c389be77f8fcd1fa1fd24851913b4
@hexfa <amir.dehdarian2009@gmail.com>
2026-01-04 23:03:17 -0800

    feat:update DataStoreRepositoryImp

--- app/src/main/java/com/example/shop/data/datastore/DataStoreRepositoryImp.kt
+++ app/src/main/java/com/example/shop/data/datastore/DataStoreRepositoryImp.kt
@@ -37,7 +37,6 @@ class DataStoreRepositoryImp @Inject constructor(
         }
 
     }
-
     override suspend fun getInt(key: String): Int? {
         return try {
             val preferencesKey = intPreferencesKey(key)
