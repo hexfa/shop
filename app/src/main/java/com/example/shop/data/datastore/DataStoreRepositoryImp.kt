e4dcd11dc7cbe97b00b8df308366528df99ecba0
@hexfa <amir.dehdarian2009@gmail.com>
2026-01-28 23:15:51 -0800

    feat:update DataStoreRepositoryImp

--- app/src/main/java/com/example/shop/data/datastore/DataStoreRepositoryImp.kt
+++ app/src/main/java/com/example/shop/data/datastore/DataStoreRepositoryImp.kt
@@ -36,7 +36,6 @@ class DataStoreRepositoryImp @Inject constructor(
             preferences[preferencesKey] = value
 
         }
-
     }
     override suspend fun getString(key: String): String? {
         return try {
