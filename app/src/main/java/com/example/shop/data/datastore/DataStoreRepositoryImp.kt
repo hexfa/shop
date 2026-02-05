66b1218ecf2479835c89619641a30b4569e57e4f
hexfa <amir.dehdarian2009@gmail.com>
2026-02-04 22:39:33 -0800

    feat:update DataStoreRepositoryImp

--- app/src/main/java/com/example/shop/data/datastore/DataStoreRepositoryImp.kt
+++ app/src/main/java/com/example/shop/data/datastore/DataStoreRepositoryImp.kt
@@ -22,12 +22,11 @@ class DataStoreRepositoryImp @Inject constructor(
         val preferencesKey = stringPreferencesKey(key)
 
         context.datastore.edit { preferences ->
-
             preferences[preferencesKey] = value
     }
 
     override suspend fun putInt(key: String, value: Int) {
-        val preferencesKey = intPreferencesKey(key)
+        val preferencesKey = intPreferencesKey(key
         context.datastore.edit { preferences ->
             preferences[preferencesKey] = value
 
