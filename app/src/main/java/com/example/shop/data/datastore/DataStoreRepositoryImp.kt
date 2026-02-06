98561dc84dadbc6abc4d8c9831ca5132847a1de5
hexfa <amir.dehdarian2009@gmail.com>
2026-02-05 23:36:42 -0800

    feat:update DataStoreRepositoryImp

--- app/src/main/java/com/example/shop/data/datastore/DataStoreRepositoryImp.kt
+++ app/src/main/java/com/example/shop/data/datastore/DataStoreRepositoryImp.kt
@@ -75,8 +75,9 @@ class DataStoreRepositoryImp @Inject constructor(
             val preferencesKey = intPreferencesKey(key)
 
             val preferences = context.datastore.data.first()
-            
+
             preferences[preferencesKey]
+            
 
 
         } catch (e: Exception) {
