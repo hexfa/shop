f7ebfc08086f9e25fff4ea155b6bf0f1993938fb
hexfa <amir.dehdarian2009@gmail.com>
2026-02-05 23:38:07 -0800

    feat:update DataStoreRepositoryImp

--- app/src/main/java/com/example/shop/data/datastore/DataStoreRepositoryImp.kt
+++ app/src/main/java/com/example/shop/data/datastore/DataStoreRepositoryImp.kt
@@ -89,6 +89,7 @@ class DataStoreRepositoryImp @Inject constructor(
             preferences[preferencesKey]
 
         } catch (e: Exception) {
+            
             e.printStackTrace()
             return null
         }
