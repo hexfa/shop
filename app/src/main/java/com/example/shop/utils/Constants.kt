4d6232c73371a798ae8b4a2da3110400298aed93
hexfa <amir.dehdarian2009@gmail.com>
2026-02-07 17:46:51 -0800

    feat:update Constants

--- app/src/main/java/com/example/shop/utils/Constants.kt
+++ app/src/main/java/com/example/shop/utils/Constants.kt
@@ -9,6 +9,7 @@ object Constants {
     const val TIMEOUT_IN_SECOND:Long=60
     const val BASE_URL="https://truelearn-digikala.iran.liara.run/api/"
     const val API_KEY=GsonBuildConfig.VERSION
-    var USER_LANGUAGE="USER_LANGUAGE"
     
+    var USER_LANGUAGE="USER_LANGUAGE"
+
 }
