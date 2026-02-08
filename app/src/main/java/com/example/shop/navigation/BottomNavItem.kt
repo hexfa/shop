3120cbc5accaca84e2ac7788a7839fe4a2b1f09a
hexfa <amir.dehdarian2009@gmail.com>
2026-02-08 14:39:47 -0800

    feat:BottomNavItem Constants

--- app/src/main/java/com/example/shop/navigation/BottomNavItem.kt
+++ app/src/main/java/com/example/shop/navigation/BottomNavItem.kt
@@ -2,9 +2,8 @@ package com.example.shop.navigation
 
 import androidx.compose.ui.graphics.painter.Painter
 
-
 data class BottomNavItem(
-    
+
     val name: String,
     val route: String,
     val selectedIcon: Painter,
