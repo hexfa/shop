a88ecc305bc5c20cf0a2948703b0527f2674d521
hexfa <amir.dehdarian2009@gmail.com>
2026-02-07 17:35:35 -0800

    feat:update Constants

--- app/src/main/java/com/example/shop/utils/Constants.kt
+++ app/src/main/java/com/example/shop/utils/Constants.kt
@@ -4,6 +4,7 @@ import com.google.gson.internal.GsonBuildConfig
 object Constants {
 
     const val ENGLISH="en"
+    
    const val PERSIAN="fa"
     const val DATASTORE_NAME="SHOP_DATA_STORE"
     const val TIMEOUT_IN_SECOND:Long=60
