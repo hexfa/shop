058ee927678c630bb660a313b98c2586e322fe56
hexfa <amir.dehdarian2009@gmail.com>
2026-02-10 20:59:27 -0800

    feat:update BottomNavItem

--- app/src/main/java/com/example/shop/navigation/BottomNavItem.kt
+++ app/src/main/java/com/example/shop/navigation/BottomNavItem.kt
@@ -5,8 +5,9 @@ import androidx.compose.ui.graphics.painter.Painter
 data class BottomNavItem(
 
     val name: String,
-    
+
     val route: String,
+    
     val selectedIcon: Painter,
     val deSelectedIcon: Painter
 )
