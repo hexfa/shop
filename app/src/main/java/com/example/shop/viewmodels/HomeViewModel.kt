80c631477588721d3728f7e0def34c84ef78eaef
hexfa <amir.dehdarian2009@gmail.com>
2026-02-15 21:59:45 -0800

    feat:update HomeViewModel

--- app/src/main/java/com/example/shop/viewmodels/HomeViewModel.kt
+++ app/src/main/java/com/example/shop/viewmodels/HomeViewModel.kt
@@ -15,6 +15,7 @@ class HomeViewModel @Inject constructor(private val homeRepo: HomeRepository):Vi
     val slider= MutableStateFlow<NetworkResult<List<Slider>>>(NetworkResult.Loading())
      fun getSlider(
 
+
      ){
          viewModelScope.launch {
 
