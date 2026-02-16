f4f90cbc898bd08b5a5855c33ea366c43b43f5f5
hexfa <amir.dehdarian2009@gmail.com>
2026-02-15 22:00:27 -0800

    feat:update HomeViewModel

--- app/src/main/java/com/example/shop/viewmodels/HomeViewModel.kt
+++ app/src/main/java/com/example/shop/viewmodels/HomeViewModel.kt
@@ -32,11 +32,12 @@ class HomeViewModel @Inject constructor(private val homeRepo: HomeRepository):Vi
 
              slider.emit(homeRepo.getSlider())
 
-             
+
 
              slider.emit(homeRepo.getSlider())
 
 
+             
 
 
 
