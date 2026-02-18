e66d718ee447b65e5fe5f56767a546fbb769278e
hexfa <amir.dehdarian2009@gmail.com>
2026-02-17 17:45:08 -0800

    feat:update HomeViewModel

--- app/src/main/java/com/example/shop/viewmodels/HomeViewModel.kt
+++ app/src/main/java/com/example/shop/viewmodels/HomeViewModel.kt
@@ -11,6 +11,7 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
+
 class HomeViewModel @Inject constructor(private val homeRepo: HomeRepository):ViewModel(){
     val slider= MutableStateFlow<NetworkResult<List<Slider>>>(NetworkResult.Loading())
      fun getSlider(
