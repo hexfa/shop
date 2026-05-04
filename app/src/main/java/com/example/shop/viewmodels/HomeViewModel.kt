44e7034fbf8c275d3a46f4746455b477683d33d4
hexfa <amir.dehdarian2009@gmail.com>
2026-05-04 23:43:02 +0330

    feat:update HomeViewModel

--- app/src/main/java/com/example/shop/viewmodels/HomeViewModel.kt
+++ app/src/main/java/com/example/shop/viewmodels/HomeViewModel.kt
@@ -20,9 +20,10 @@ class HomeViewModel @Inject constructor(private val homeRepo: HomeRepository):Vi
      fun getSlider(
 
      ){
-         
+
 
          viewModelScope.launch {
+             
              slider.value = NetworkResult.Loading()
              slider.emit(homeRepo.getSlider())
              slider.emit(homeRepo.getSlider())
